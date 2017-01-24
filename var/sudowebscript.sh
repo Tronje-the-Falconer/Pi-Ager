b638487f653b7b82a50a98e1abfe61850d1a1768
Tronje-the-Falconer <club@kording-web.de>
2017-01-24 08:51:37 +0100

    Update sudowebscript.sh

--- var/sudowebscript.sh
+++ var/sudowebscript.sh
@@ -3,46 +3,55 @@
 # sudo web script allowing user www-data to run commands with root privilegs
 
 case "$1" in
-    startrss)
+    startrss) #Starten von Rss.py
         python /opt/RSS1.0/Rss.py > /dev/null 2>/dev/null &
     ;;
-    stoprss)
+    stoprss) #Stoppen von Rss.py
         pkill -f Rss.py
     ;;
-    startsalami)
-        python /opt/RSS1.0/salami.py > /dev/null 2>/dev/null &
-    ;;
-    stopsalami)
-        pkill -f salami.py
-    ;;
-    r22)
-        /usr/local/bin/gpio -g read 22
-    ;;
-    r27)
-        /usr/local/bin/gpio -g read 27
-    ;;
-    r18)
-        /usr/local/bin/gpio -g read 18
-    ;;
-    r23)
-        /usr/local/bin/gpio -g read 23
-    ;;
-    r24)
-        /usr/local/bin/gpio -g read 24
-    ;;
-    grsalami)
-        ps ax | grep -v grep | grep salami.py
-    ;;
-    grrss)
+    grrss) #Überprüfen von Rss.py
         ps ax | grep -v grep | grep Rss.py
     ;;
-    reb)
+    startsalami) #Starten von salami.py
+        python /opt/RSS1.0/salami.py > /dev/null 2>/dev/null &
+    ;;
+    stopsalami) #Stoppen von salami.py
+        pkill -f salami.py
+    ;;
+    grsalami) #Überprüfen von salami.py
+        ps ax | grep -v grep | grep salami.py
+    ;;
+    startreifetab) #Starten von Reifetab.py
+        sudo python /home/pi/RSS1.0/Reifetab.py > /dev/null 2>/dev/null &
+    ;;
+    stopstopreifetab) #Stoppen von Reifetab.py
+        pkill -f Reifetab.py
+    ;;
+    grreifetab) #Überprüfen von Reifetab.py
+        ps ax | grep -v grep | grep Reifetab.py
+    ;;
+    r22) #Ansteuern von GPIO22 Kühlschrankkompressor
+        /usr/local/bin/gpio -g read 22
+    ;;
+    r27)#Ansteuern von GPIO27 Heizkabel
+        /usr/local/bin/gpio -g read 27
+    ;;
+    r18)#Ansteuern von GPIO18 Umluftventilator
+        /usr/local/bin/gpio -g read 18
+    ;;
+    r23)#Ansteuern von GPIO23 Austauschlüfter
+        /usr/local/bin/gpio -g read 23
+    ;;
+    r24)#Ansteuern von GPIO24 Luftbefeuchter
+        /usr/local/bin/gpio -g read 24
+    ;;
+    reb) # reboot
         reboot
     ;;
-    shut)
+    shut) #Shutdown 
         shutdown -h now
     ;;
-    *) echo "ERROR: invalid parameter: $1 (for $0)"; exit 1
+    *) echo "ERROR: invalid parameter: $1 (for $0)"; exit 1 #Fehlerbehandlung
     ;;
 esac
 
