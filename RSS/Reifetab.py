24cef1cfef3b179b5d2a19650863540b39f9906c
Tronje-the-Falconer <club@kording-web.de>
2017-01-29 21:14:13 +0100

    Add files via upload
    Strukturiert nach RSS und csv in Unterordner csv

---
+++ RSS/Reifetab.py
@@ -0,0 +1,145 @@
+import sys
+import os;
+import json;
+import glob;
+import time;
+import datetime
+import csv
+from datetime import timedelta
+
+global Reifetab
+
+PATH = '/var/www/html';
+SETTINGS_FILE = PATH+'/settings.json';
+TABELS_FILE=PATH+'/tabels.json';
+filename=PATH+'/logfile.txt' ;
+
+def readSettings():
+    global TABELS_FILE;
+    s = None;
+    with open(TABELS_FILE, 'r') as file:
+        s = file.read();
+    data = json.loads(s);
+    return data
+
+def write_settings(mod, temp, hum, tempoff, tempon, tempoff1, tempon1, temphyston, temphystoff, humhyston, humhystoff, humdelay, sensortype):
+    global SETTINGS_FILE;
+
+    s = json.dumps({"mod":mod, "temp":temp, "hum":hum, "tempoff":tempoff, "tempon":tempon, "tempoff1":tempoff1, "tempon1":tempon1, "temphyston":temphyston, "temphystoff":temphystoff, "humhyston":humhyston, "humhystoff":humhystoff, "humdelay":humdelay, "sensortype":sensortype 'date':int(time.time())});
+    with open(SETTINGS_FILE, 'w') as file:
+        file.write(s);
+
+Reifetabs= readSettings();     
+tabelle=Reifetabs['Reifetab']
+datei =tabelle +'.csv';
+target = open(filename, 'a')
+target.write ("\n" +time.strftime('%d.%m.%Y  %H:%M') + ': Kontrolle der Werte von '+tabelle +' uebernommen')
+target.close()
+
+f=open('/opt/RSS/csv/'+datei,"rb")
+reader=csv.reader(f)
+rownum=0
+dauer=0
+
+for row in reader:
+    colnum=0
+    for col in row:
+        if rownum>0:
+            if colnum==12:
+                dauer=dauer + int(col)
+            colnum+=1
+    rownum+=1
+totper=rownum-1
+totdauer=dauer
+
+f=open('/opt/RSS/csv/' + datei,"rb")
+reader=csv.reader(f)
+
+mod = 4
+temp = 5
+hum = 75
+tempoff = 3000
+tempon = 600
+tempoff1 = 82800
+tempon1 = 3600
+temphyston = 1
+temphystoff = 0.5
+humhyston = 5
+humhystoff = 2
+humdelay = 5
+dauer = 86400
+
+t=86400 ; #Anzahl der Sek. in einem Tag
+#t=1 ; #zum testen; ein Tag vergeht in einer Sekunde
+rownum=0
+for row  in reader:
+    colnum=0  
+    if rownum==0:
+        header=row
+    else:
+        if rownum==1 :
+            print "****************************************************************************"
+            print time.strftime('%d.%m.%Y  %H:%M') + ": Initialwerte fur Periode 1/"+str(totper) + " vom Reifeprogramm " + tabelle +":"
+            print
+            target = open(filename, 'a')
+            target.write("\n"+ "****************************************************************************")                 
+            target.write("\n" + time.strftime('%d.%m.%Y  %H:%M') + ": Initialwerte fur Periode 1/"+str(totper) + " vom Reifeprogramm " + tabelle +":")
+            target.write("\n")
+            target.close()
+            finaltime=datetime.datetime.now()+timedelta(days=totdauer)
+        else:
+            print "*****************************************************************************"
+            print time.strftime('%d.%m.%Y  %H:%M') + ": Veraenderungen fur Periode " + str(rownum) + "/" + str(totper) + " vom Reifeprogramm " + tabelle +":"
+            print
+            target = open(filename, 'a')
+            target.write("\n"+ "****************************************************************************")                 
+            target.write("\n" + time.strftime('%d.%m.%Y  %H:%M')  +": Veraenderungen fur Periode " + str(rownum) + "/" + str(totper)+ " vom Reifeprogramm " + tabelle +":")
+            
+            target.close()
+        colnum=0
+        wert=0
+        for col in row:
+        colnumname=header[colnum]
+        if colnumname=="days":
+            dauer=int(col)*t
+        if col<>"":
+            if colnumname=='temphystoff':
+                temphystoff=float(col)
+            else:
+                wert=float(col)          
+                exec('%s=%d') % (colnumname,wert)
+                print '%-12s:%s' % (colnumname,wert)
+                target = open(filename, 'a')
+                target.write("\n" +colnumname + " \t" +col)
+                target.close()
+            colnum+=1
+    rownum+=1
+
+    write_settings (mod, temp, hum, tempoff, tempon, tempoff1, tempon1, temphyston, temphystoff, humhyston, humhystoff, humdelay, sensortype);
+    if rownum>1 :
+        endtime=datetime.datetime.now()+timedelta(days=dauer/t)
+        print
+        if rownum<totper+1:
+            print "Naechste Aenderung der Werte: " + endtime.strftime('%d.%m.%Y  %H:%M')
+            target = open(filename, 'a')
+            target.write("\n" +"Naechste Aenderung der Werte: " + endtime.strftime('%d.%m.%Y  %H:%M'))
+            target.close()
+            print "Programmende: " +finaltime.strftime('%d.%m.%Y  %H:%M')
+            target = open(filename, 'a')
+            target.write("\n" + "Programmende: " +finaltime.strftime('%d.%m.%Y  %H:%M'))
+            target.close()      
+        if rownum==totper+1 :
+            print "Nach Programmende funktioniert der Reifeschrank weiter mit den letzten Werten"
+            target = open(filename, 'a')
+            target.write("\n" + tabelle +" beendet die Kontrolle."+"\n"+"Der Reifeschrank funktioniert weiter mit den letzten Werten.")
+            target.close()     
+        print "*****************************************************************************"
+        target = open(filename, 'a')
+        target.write("\n" + "*****************************************************************************")
+        target.close()      
+        if rownum<=totper:
+            time.sleep(dauer)
+
+f.close()
+
+sys.exit(0)
---
+++ RSS/conf.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-1 -*-
+#Importieren der Module
+
+import os;
+import json;
+import glob;
+import time;
+
+# Konstanten ############################################################################################################
+########################################################################################################################
+
+PATH = '/var/www/html';
+SETTINGS_FILE = PATH+'/settings.json';
+VERBOSE = True;
+
+#Standard Hysteresewerte
+temphyston = float(1.5);
+temphystoff = float(0.5)
+humhyston = float(5);
+humhystoff = float(2);
+
+#Funktionen##############################################################################################################
+#########################################################################################################################
+
+def writeVerbose(s, newLine=False):
+    global VERBOSE;
+    
+    if(VERBOSE):
+        print(s);
+        if(newLine is True):
+            print('');
+
+def write_settings(mod, temp, hum, tempoff, tempon, tempoff1, tempon1, temphyston, temphystoff, humhyston, humhystoff, humdelay, sensortype):
+    global SETTINGS_FILE;
+
+    s = json.dumps({"mod":mod, "temp":temp, "hum":hum, "tempoff":tempoff, "tempon":tempon, "tempoff1":tempoff1, "tempon1":tempon1, "temphyston":temphyston, "temphystoff":temphystoff, "humhyston":humhyston, "humhystoff":humhystoff,"humdelay":humdelay, 'sensortype':sensortype, 'date':int(time.time())});
+    with open(SETTINGS_FILE, 'w') as file:
+        file.write(s);
+
+# Hauptprogramm #################################################################################################################
+########################################################################################################################
+
+#os.system('clear'); # Bildschirm löschen
+writeVerbose('************************************************************');
+
+#Vriablen Eingabe
+mod = int(raw_input('Kühlmodus (1) Heizmodus(2) Automodus (3) Erweiteter Automodus(4):'))
+temp = float(raw_input('Bitte geben sie die SollTemperatur ein (2-25):'))
+hum = float(raw_input('Bitte geben sie die SollLuftfeuchtigkeit ein(40-95):'))
+humdelay = int(raw_input('Bitte geben sie die Verzögerung für den Befeuchter in Minuten an :'))
+writeVerbose('');
+tempoff = int(raw_input('Bitte geben sie den Abstand für die Luftumwelzung in Stunden ein :'))
+tempon = int(raw_input('Bitte geben sie die Einschaltzeit für die Luftumerlzung in Minuten ein:'))
+writeVerbose('');
+tempoff1 = int(raw_input('Bitte geben sie den Abstand für den Luftaustausch in Stunden ein :'))
+tempon1 = int(raw_input('Bitte geben sie die Einschaltzeit für den Luftaustausch in Minuten ein:'))
+writeVerbose('');
+hystdef = int(raw_input('Wollen sie die Hysteresewerte anpassen (1)Ja (2)Nein ?'));
+if hystdef == 1:
+        temphyston = float(raw_input('Bitte geben sie den Hysteresewert für den Temperatureinschaltwert an:'))
+        temphystoff = float(raw_input('Bitte geben sie den Hystereswert für den Temperaturausschaltwert an:'))
+        writeVerbose('');
+        humhyston = float(raw_input('Bitte geben sie den Hysteresewert für den Lft-einschaltwert an:'))
+        humhystoff = float(raw_input('Bitte geben sie den Hysteresewert für den Lft-ausschaltwert an:'))
+tempon = tempon*60
+tempoff = tempoff*3600-tempon
+tempon1 = tempon1*60
+tempoff1 = tempoff1*3600-tempon1
+sensortype = int(raw_input('DHT11 (1) DHT22(2) SHT75 (3):'))
+
+write_settings (mod, temp, hum, tempoff, tempon, tempoff1, tempon1, temphyston, temphystoff, humhyston, humhystoff, humdelay, sensortype,);
+writeVerbose('************************************************************');
+writeVerbose('**************Die Änderungen wurden gespeichert*************');
+writeVerbose('************************************************************');
