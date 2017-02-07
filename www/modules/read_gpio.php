bb7062a75a503ced7e077583778d696c2b8352a2
Tronje-the-Falconer <club@kording-web.de>
2017-02-07 06:26:38 +0100

    Update read_gpio.php
    zeile 11 korigiert

--- www/modules/read_gpio.php
+++ www/modules/read_gpio.php
@@ -1,16 +1,16 @@
 <?php
     # Status auslesen
-    $val22 = shell_exec('sudo /var/sudowebscript.sh read22'); #Kühlung
+    $val22 = shell_exec('sudo /var/sudowebscript.sh read22'); #KÃ¼hlung
     $val27 = shell_exec('sudo /var/sudowebscript.sh read27'); #Heizung
     $val24 = shell_exec('sudo /var/sudowebscript.sh read24'); #luftbefeuchter
     $val18 = shell_exec('sudo /var/sudowebscript.sh read18'); #Umluft
     $val23 = shell_exec('sudo /var/sudowebscript.sh read23'); #Abluft
 
-    #Prüfen ob Programme laufen
+    #PrÃ¼fen ob Programme laufen
     $valtab = shell_exec('sudo /var/sudowebscript.sh grepreifetab'); #Reifetab.py
-    $valrss = shell_exec('sudo /var/sudowebscript.sh greprss.py'); #Rss.py
+    $valrss = shell_exec('sudo /var/sudowebscript.sh greprss'); #Rss.py
 
-    #Schaltzustände setzen
+    #SchaltzustÃ¤nde setzen
     if($val22 == 0) {
         $cool = 'images/led-on-green-20x20.png';
         }
