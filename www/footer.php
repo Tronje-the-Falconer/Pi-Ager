9868b3891bd9efa9c2ecd4090b232523a620b21e
Tronje-the-Falconer <club@kording-web.de>
2017-02-07 08:16:35 +0100

    Update footer.php
    PiVersion auslesbar

--- www/footer.php
+++ www/footer.php
@@ -6,7 +6,10 @@
                 <div class="art-content-layout">
                     <div class="art-content-layout-row">
                         <div class="art-layout-cell layout-item-0" style="width: 50%">
-                            <p>Running Engine<br>RaspberryPI 2B+</p>
+                            <?PHP
+                              include   'modules/read_pirevision.php';
+                              print '<p>Running Engine<br>' + str ($piversion)+ '</p>'
+                            ?>
                         </div>
                         <div class="art-layout-cell layout-item-0" style="width: 50%">
                             <p style="text-align: right;">Version 2.01<br><a href="https://www.grillsportverein.de/forum/threads/reifeschranksteuerung-per-raspberry-pi-tutorial.231649/" target="_blank">www.grillsportverein.de</a></p>
