919c9d65336afc6d684bc250eb346b65c874fecf
Tronje-the-Falconer <club@kording-web.de>
2017-02-08 13:18:57 +0100

    Update footer.php
    Link auf Changelog.php und ausgelesene version (header.php) verwenden

--- www/footer.php
+++ www/footer.php
@@ -14,7 +14,15 @@
                             ?>
                         </div>
                         <div class="art-layout-cell layout-item-0" style="width: 50%">
-                            <p style="text-align: right;">Version 2.01<br><a href="https://www.grillsportverein.de/forum/threads/reifeschranksteuerung-per-raspberry-pi-tutorial.231649/" target="_blank">www.grillsportverein.de</a></p>
+                            <p style="text-align: right;">
+                                <a href="changelog.php">
+                                    <?php
+                                        print $rssversion;
+                                    ?>
+                                </a>
+                                <br>
+                                <a href="https://www.grillsportverein.de/forum/threads/reifeschranksteuerung-per-raspberry-pi-tutorial.231649/" target="_blank">www.grillsportverein.de</a>
+                            </p>
                         </div>
                     </div>
                 </div>
