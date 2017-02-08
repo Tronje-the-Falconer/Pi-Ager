2c5ebac9c2d3b8d0f9d87ffca64006d094202716
Tronje-the-Falconer <club@kording-web.de>
2017-02-08 11:51:17 +0100

    Update changelog.php

--- www/changelog.php
+++ www/changelog.php
@@ -1,18 +1,17 @@
                                 <?php
                                     include 'header.php';      // Template-Kopf und Navigation
-                                    include 'modules/clear_logfile.php';
                                 ?>
-                                <h2 class="art-postheader">Logeinträge</h2>
+                                <h2 class="art-postheader">Changelog</h2>
                                 <div class="hg_container" style="text-align: left;">
                                     <form  method="post">
                                         <table style="width: 100%">
                                             <tr>
                                                 <td>
                                                     <?php
-                                                        if (is_file($logfile)) {
-                                                            echo 'Dateiprüfung: '.$logfile.'<br />';
-                                                            echo 'Dateigröße: '.filesize($logfile).' bytes<br />';
-                                                            $mtime = filemtime($logfile);
+                                                        if (is_file($changelogfile)) {
+                                                            echo 'Dateiprüfung: '.$changelogfilefile.'<br />';
+                                                            echo 'Dateigröße: '.filesize($changelogfilefile).' bytes<br />';
+                                                            $mtime = filemtime($changelogfilefile);
                                                             print 'Zuletzt geändert am: ';
                                                             print date('d M Y, H:i:s', $mtime);
                                                             print ' Uhr<br />';
@@ -21,29 +20,21 @@
                                                         else {
                                                             print '<img src="images/no_small.png"> Datei ist nicht vorhanden<br />';
                                                         }
-                                                        if (is_readable($logfile)) {
+                                                        if (is_readable($changelogfilefile)) {
                                                             print '<img src="images/yes_small.png"> Datei ist lesbar<br />';
                                                         }
                                                         else {
                                                             print '<img src="images/no_small.png"> Datei ist nicht lesbar<br />';
                                                         }
-                                                        if (is_writable($logfile)) {
-                                                            print '<img src="images/yes_small.png"> Datei ist schreibbar<br />';
-                                                        }
-                                                        else {
-                                                            print '<img src="images/no_small.png"> Datei ist nicht schreibbar<br />';
-                                                        }
                                                     ?>
                                                 </td>
-                                                <td>&nbsp;</td>
-                                                <td><button class="art-button" name="clear_logfile" onclick="return confirm('Alle Logfiledaten löschen?');">Daten löschen</button></td>
                                             </tr>
                                         </table>
                                     </form>
                                     <hr>
                                     <!----------------------------------------------------------------------------------------Logeinträge-->
                                     <?php
-                                        $f = file('logfile.txt');
+                                        $f = file('changelog.txt');
                                         foreach($f as $file) {
                                             echo '<br />'. $file;
                                         }
