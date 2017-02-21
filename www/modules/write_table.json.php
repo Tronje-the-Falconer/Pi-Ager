3f9dac85b7223617896c325fcbb2688bb20bb238
GSV-Ha-Ma <hm.schell@gmx.de>
2017-02-21 17:59:48 +0100

    Update write_table.json.php
    tabels nach tables in Zeile 7 umbenannt

--- www/modules/write_table.json.php
+++ www/modules/write_table.json.php
@@ -4,7 +4,7 @@
         $timestamp = time();
         $array = array( 'Reifetab' => $_POST['Reifetab'],'date' => $timestamp);
         $jsoninput = json_encode($array);
-        file_put_contents('tabels.json', $jsoninput);
+        file_put_contents('tables.json', $jsoninput);
 
         #3 Sekunden anzeigen, dass gespeichert wurde
         print '<p id="info-message" style="color: #ff0000; font-size: 20px;"><b>Auswahl gespeichert</b></p>
