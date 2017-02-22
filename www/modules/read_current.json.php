f51c72f0372383a16df4a5e8c112cf6d611c1562
GSV-Steinbacher <d.s.o@gmx.com>
2017-02-22 19:13:03 +0100

    Update read_current.json.php

--- www/modules/read_current.json.php
+++ www/modules/read_current.json.php
@@ -2,7 +2,7 @@
     # current.json auslesen um aktuelle werte von temperatur und luftfeuchtigkeit anzuzeigen
     $API = file_get_contents('current.json');
     $array = json_decode($API, true);
-    $temp_float = $array['temperatur'];
-    $hum_float = $array['luftfeuchtigkeit'];
+    $temp_float = round($array['temperatur'], 1);
+    $hum_float = round($array['luftfeuchtigkeit'], 1);
     $data_timestamp = $array['date'];
 ?>
