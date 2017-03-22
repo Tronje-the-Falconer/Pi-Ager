0edf55a9e10db353a35ceb3a500d859302b18d26
Tronje-the-Falconer <club@kording-web.de>
2017-03-22 14:27:44 +0100

    Update read_settings_json.php

--- www/modules/read_settings_json.php
+++ www/modules/read_settings_json.php
@@ -1,8 +1,8 @@
 <?php
     # settings.json auslesen um sollwerte wieder zu geben
     $SET = file_get_contents('settings.json');
-    $array_settings_json = json_decode($SET, true);
-    $modus = $array_settings_json['modus'];
+    $data_settingsjsonfile = json_decode($SET, true);
+    $modus = $data_settingsjsonfile['modus'];
     if ($modus  ==  0) {
         $modus_name = '- '._('cooling');
         $checked_0 = 'checked="checked"';
@@ -38,39 +38,12 @@
     else {
         $checked_4 = '';
     }
-    $sensortype = $array_settings_json['sensortype'];
-    if ($sensortype == 1) {
-        $sensorname = 'DHT11';
-        $checked_sens_1 = 'checked="checked"';
-    }
-    else {
-        $checked_sens_1 = '';
-    }
-    if ($sensortype == 2) {
-        $sensorname = 'DHT22';
-        $checked_sens_2 = 'checked="checked"';
-    }
-    else {
-        $checked_sens_2 = '';
-    }
-    if ($sensortype == 3) {
-        $sensorname = 'SHT75';
-        $checked_sens_3 = 'checked="checked"';
-    }
-    else {
-        $checked_sens_3 = '';
-    }
-    $setpoint_temperature = $array_settings_json['setpoint_temperature'];
-    $setpoint_humdity = $array_settings_json['setpoint_humdity'];
-    $delay_humidify = $array_settings_json['delay_humidify'];
-    $circulation_air_duration = $array_settings_json['circulation_air_duration'];
-    $circulation_air_period = $array_settings_json['circulation_air_period'];
-    $exhaust_air_duration = $array_settings_json['exhaust_air_duration'];
-    $exhaust_air_period = $array_settings_json['exhaust_air_period'];
-    $switch_on_cooling_compressor = $array_settings_json['switch_on_cooling_compressor'];
-    $switch_off_cooling_compressor = $array_settings_json['switch_off_cooling_compressor'];
-    $switch_on_humidifier = $array_settings_json['switch_on_humidifier'];
-    $switch_off_humidifier = $array_settings_json['switch_off_humidifier'];
+    $setpoint_temperature = $data_settingsjsonfile['setpoint_temperature'];
+    $setpoint_humdity = $data_settingsjsonfile['setpoint_humdity'];
+    $circulation_air_duration = $data_settingsjsonfile['circulation_air_duration'];
+    $circulation_air_period = $data_settingsjsonfile['circulation_air_period'];
+    $exhaust_air_duration = $data_settingsjsonfile['exhaust_air_duration'];
+    $exhaust_air_period = $data_settingsjsonfile['exhaust_air_period'];
     $circulation_air_period = $circulation_air_period/60;
     $circulation_air_duration = $circulation_air_duration/60;
     $exhaust_air_period = $exhaust_air_period/60;
