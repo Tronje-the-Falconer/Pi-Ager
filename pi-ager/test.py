b0d84da19f9ffdbd8a7c7134a07889d14c07cee9
Tronje-the-Falconer <club@kording-web.de>
2017-05-07 21:29:48 +0200

    Reference Units ergänzt

--- pi-ager/test.py
+++ pi-ager/test.py
@@ -3,9 +3,21 @@ import sys
 import RPi.GPIO as GPIO
 from hx711 import Scale
 
-scale = Scale(gain=128)
+scale = Scale(gain=128) #32, 64, 128
 
 scale.setReferenceUnit(60)
+# 10KG China Zelle:
+#   Gain 32 -> 60
+#   Gain 128 -> 205
+# 20kg China Zelle:
+#   Gain 32 -> 
+#   Gain 128 -> 102
+# 50kg Edelstahl Zelle:
+#   Gain 32 -> 
+#   Gain 128 -> 74
+# 20kg Edelstahl Zelle:
+#   Gain 32 -> 
+#   Gain 128 -> 186
 
 scale.reset()
 scale.tare()
