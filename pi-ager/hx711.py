5c28229bb97b17dade59d87486b034c88402afb9
Tronje-the-Falconer <club@kording-web.de>
2017-05-07 20:29:31 +0200

    Update hx711.py
    Added Scale class

--- pi-ager/hx711.py
+++ pi-ager/hx711.py
@@ -115,3 +115,64 @@ class HX711:
     def reset(self):
         self.powerDown()
         self.powerUp()
+
+class Scale:
+    def __init__(self, source=None, samples=20, spikes=4, sleep=0.1):
+
+        self.source = source or HX711()
+        self.samples = samples
+        self.spikes = spikes
+        self.sleep = sleep
+        self.history = []
+
+    def newMeasure(self):
+        value = self.source.getWeight()
+        self.history.append(value)
+
+    def getMeasure(self):
+        """Useful for continuous measurements."""
+        self.newMeasure()
+        # cut to old values
+        self.history = self.history[-self.samples:]
+
+        avg = statistics.mean(self.history)
+        deltas = sorted([abs(i-avg) for i in self.history])
+
+        if len(deltas) < self.spikes:
+            max_permitted_delta = deltas[-1]
+        else:
+            max_permitted_delta = deltas[-self.spikes]
+
+        valid_values = list(filter(
+            lambda val: abs(val - avg) <= max_permitted_delta, self.history
+        ))
+
+        avg = statistics.mean(valid_values)
+
+        return avg
+
+    def getWeight(self, samples=None):
+        """Get weight for once in a while. It clears history first."""
+        self.history = []
+
+        [self.newMeasure() for i in range(samples or self.samples)]
+
+        return self.getMeasure()
+
+    def tare(self, times=25):
+        self.source.tare(times)
+
+    def setOffset(self, offset):
+        self.source.setOffset(offset)
+
+    def setReferenceUnit(self, reference_unit):
+        self.source.setReferenceUnit(reference_unit)
+
+    def powerDown(self):
+        self.source.powerDown()
+
+    def powerUp(self):
+        self.source.powerUp()
+
+    def reset(self):
+        self.source.reset()
