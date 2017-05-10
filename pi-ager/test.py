a7d3acf62f93465b9453bf3d0e8d5b9278b74ff6
Tronje-the-Falconer <club@kording-web.de>
2017-05-10 21:30:41 +0200

    Update test.py

--- pi-ager/test.py
+++ pi-ager/test.py
@@ -2,9 +2,12 @@
 import sys
 import RPi.GPIO as GPIO
 import time
+import datetime
 from hx711 import Scale
 
 scale = Scale(gain=128) #32[Channel B], 64 [Channel A], 128 [Channel A]
+
+
 
 scale.setReferenceUnit(60)
 # 10KG China Zelle:
@@ -22,19 +25,25 @@ scale.setReferenceUnit(60)
 
 scale.reset()
 scale.tare()
-last_minute = datetime.datetime.fromtimestamp(ts).strftime('%M')
+
+tsstart = time.time()
+last_minute = datetime.datetime.fromtimestamp(tsstart).strftime('%M')
+
 
 while True:
 
     try:
         value = scale.getMeasure()
+        formated_value = ("{0: 4.4f}".format(value))
+        ts = time.time()
         timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
         current_minute = datetime.datetime.fromtimestamp(ts).strftime('%M')
-        print("{0: 4.4f}".format(val))
+        print (timestamp + ' ; ' + str(formated_value) + ' gr ; Minute: ' + current_minute)
         
-        if current_minute = last_minute:
+        if current_minute != last_minute:
             with open("scale.txt", "a") as fh:
-	            fh.write(timestamp + ' ; ' + str(value))
+                fh.write(timestamp + ' ; ' + str(formated_value))
+                fh.write('\r\n')
             last_minute = current_minute
         
     except (KeyboardInterrupt, SystemExit):
