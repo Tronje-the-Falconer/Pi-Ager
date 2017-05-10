fd095cf94447bdfc21a93404a7034845636ddf45
Tronje-the-Falconer <club@kording-web.de>
2017-05-10 11:04:10 +0200

    Ausgabe in Datei ergänzt
    Es wird jede Minute der aktuelle Wert mit Zeitstempel in eine Datei geschrieben.

--- pi-ager/test.py
+++ pi-ager/test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 import RPi.GPIO as GPIO
+import time
 from hx711 import Scale
 
 scale = Scale(gain=128) #32[Channel B], 64 [Channel A], 128 [Channel A]
@@ -21,13 +22,21 @@ scale.setReferenceUnit(60)
 
 scale.reset()
 scale.tare()
+last_minute = datetime.datetime.fromtimestamp(ts).strftime('%M')
 
 while True:
 
     try:
-        val = scale.getMeasure()
+        value = scale.getMeasure()
+        timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+        current_minute = datetime.datetime.fromtimestamp(ts).strftime('%M')
         print("{0: 4.4f}".format(val))
-
+        
+        if current_minute = last_minute:
+            with open("scale.txt", "a") as fh:
+	            fh.write(timestamp + ' ; ' + str(value))
+            last_minute = current_minute
+        
     except (KeyboardInterrupt, SystemExit):
         GPIO.cleanup()
         sys.exit()
