15bd212ace0eb769cda126cb4bb4bd6ec4c9a102
Tronje-the-Falconer <club@kording-web.de>
2017-10-10 08:45:36 +0200

    Delete read_current_json.php

--- www/modules/read_current_json.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    # config/current.json auslesen um aktuelle werte von temperatur und luftfeuchtigkeit anzuzeigen
-    $current_json_file = file_get_contents('config/current.json');
-    $array_current_json = json_decode($current_json_file, true);
-    $sensor_temperature = round($array_current_json['sensor_temperature'], 1);
-    $sensor_humidity = round($array_current_json['sensor_humidity'], 1);
-    $current_json_timestamp_last_change = $array_current_json['last_change'];
-?>
