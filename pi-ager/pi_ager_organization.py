bc7432c145cac9003737199c0e99050405345fe4
Tronje-the-Falconer <club@kording-web.de>
2017-11-15 21:11:19 +0100

    gpio cleanup nur bei gesetzten GPIO

--- pi-ager/pi_ager_organization.py
+++ pi-ager/pi_ager_organization.py
@@ -17,9 +17,24 @@ def goodbye():
 # Function cleanup
 def cleanup():
     global logger
-    logstring = _('running cleanup script') + '...'
-    logger.info(logstring)
-    gpio.cleanup() # GPIO zuruecksetzen
-    logstring = _('cleanup complete') + '.'
+    
+    
+    gpio_exhausting_air_usage = gpio.gpio_function(pi_ager_names.gpio_exhausting_air)
+    print (gpio_exhausting_air_function)
+    gpio_circulating_air_usage = gpio.gpio_function(pi_ager_names.gpio_circulating_air)
+    gpio_cooling_compressor_usage = gpio.gpio_function(pi_ager_names.gpio_cooling_compressor)
+    gpio_dehumidifier_usage = gpio.gpio_function(pi_ager_names.gpio_dehumidifier)
+    gpio_heater_usage = gpio.gpio_function(pi_ager_names.gpio_heater)
+    gpio_humidifier_usage = gpio.gpio_function(pi_ager_names.gpio_humidifier)
+    gpio_light_usage = gpio.gpio_function(pi_ager_names.gpio_light)
+    gpio_uv_usage = gpio.gpio_function(pi_ager_names.gpio_uv)
+    
+    if (gpio_uv_usage != -1 or gpio_light_usage != -1 or gpio_humidifier_usage != -1 or gpio_heater_usage != -1 or gpio_dehumidifier_usage != -1 or gpio_cooling_compressor_usage != -1 or gpio_circulating_air_usage != -1 or gpio_exhausting_air_function != -1):
+        logstring = _('running cleanup script') + '...'
+        logger.info(logstring)
+        gpio.cleanup() # gpio zuruecksetzen
+        logstring = _('cleanup complete') + '.'
+    else:
+        logstring = _('nothing to cleanup') + '.'
     logger.info(logstring)
     logger.info(pi_ager_names.logspacer)
