97ec956d492223d64f31f7e325fdfa1969268d1d
Tronje-the-Falconer <club@kording-web.de>
2017-12-17 18:59:37 +0100

    floatkonvertierung bei reference unit

--- www/modules/read_config_db.php
+++ www/modules/read_config_db.php
@@ -21,14 +21,14 @@
     $failure_humidity_delta = get_table_value($config_settings_table, $failure_humidity_delta_key);
     $failure_temperature_delta = get_table_value($config_settings_table, $failure_temperature_delta_key);
     
-    $referenceunit_scale1 = get_table_value($settings_scale1_table,$referenceunit_key);
+    $referenceunit_scale1 = number_format(floatval(get_table_value($settings_scale1_table,$referenceunit_key)), 1, '.', '');
     $measuring_interval_scale1 = get_table_value($settings_scale1_table,$scale_measuring_interval_key);
     $measuring_duration_scale1 = get_table_value($settings_scale1_table,$measuring_duration_key);
     $saving_period_scale1 = get_table_value($settings_scale1_table,$saving_period_key);
     $samples_scale1 = get_table_value($settings_scale1_table,$samples_key);
     $spikes_scale1 = get_table_value($settings_scale1_table,$spikes_key);
     
-    $referenceunit_scale2 = get_table_value($settings_scale2_table,$referenceunit_key);
+    $referenceunit_scale2 = number_format(floatval(get_table_value($settings_scale2_table,$referenceunit_key)), 1, '.', '');
     $measuring_interval_scale2 = get_table_value($settings_scale1_table,$scale_measuring_interval_key);
     $measuring_duration_scale2 = get_table_value($settings_scale1_table,$measuring_duration_key);
     $saving_period_scale2 = get_table_value($settings_scale1_table,$saving_period_key);
