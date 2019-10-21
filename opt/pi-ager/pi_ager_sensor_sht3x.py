12273479295b91cceb5450c3329083d76fa2ad34
DerBurgermeister <DerBurgermeister@pi-ager.org>
2019-10-21 21:07:58 +0200

    sht3x

--- opt/pi-ager/pi_ager_sensor_sht3x.py
+++ opt/pi-ager/pi_ager_sensor_sht3x.py
@@ -17,8 +17,7 @@ import inspect
 from pi_ager_cl_sensor_type import cl_fact_main_sensor_type
 from pi_ager_cl_i2c_bus import cl_fact_i2c_bus_logic
 from pi_ager_cx_exception import *
-import pi_ager_cl_ab_sensor 
-
+from pi_ager_cl_ab_sensor import cl_ab_temp_sensor, cl_ab_humitity_sensor
 
 class cl_main_sensor_sht3x(cl_ab_temp_sensor, cl_ab_humitity_sensor):
     
