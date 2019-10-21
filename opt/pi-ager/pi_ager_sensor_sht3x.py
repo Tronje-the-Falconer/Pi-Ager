a8e8cfbef4b2ae20c32268411c9d8a19e2050a0e
DerBurgermeister <DerBurgermeister@pi-ager.org>
2019-10-21 18:16:07 +0200

    sht3x

--- opt/pi-ager/pi_ager_sensor_sht3x.py
+++ opt/pi-ager/pi_ager_sensor_sht3x.py
@@ -13,9 +13,9 @@ __status__ = "Production"
 
 from abc import ABC, abstractmethod
 import inspect
+import pi_ager_cl_sensor
 
 from pi_ager_cl_sensor_type import cl_fact_main_sensor_type
-from pi_ager_cl_sensor import cl_fact_main_sensor
 from pi_ager_cl_i2c_bus import cl_fact_i2c_bus_logic
 from pi_ager_cx_exception import *
 
