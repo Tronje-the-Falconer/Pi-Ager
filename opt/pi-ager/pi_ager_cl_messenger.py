b04ad06208c8559028bc3ed01eb66a4d4cee7ad2
DerBurgermeister <DerBurgermeister@pi-ager.org>
2019-10-21 11:53:33 +0200

    Send mail

--- opt/pi-ager/pi_ager_cl_messenger.py
+++ opt/pi-ager/pi_ager_cl_messenger.py
@@ -8,7 +8,7 @@ import pi_ager_names
 import pi_ager_logging
 from pi_ager_cl_database import cl_fact_database_config
 from pi_ager_cl_alarm import cl_fact_logic_alarm
-from pi_ager_cl_send_email import cl_fact_logic_send_email, cl_logic_send_mail
+from pi_ager_cl_send_email import cl_fact_logic_send_email, cl_logic_send_email
                              
 from pi_ager_cx_exception import *
 from _ast import Pass
