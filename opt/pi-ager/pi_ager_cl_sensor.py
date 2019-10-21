2b81b4ca978fd5a860d616fde0d6eee4a060e43a
DerBurgermeister <DerBurgermeister@pi-ager.org>
2019-10-21 17:50:56 +0200

    sht3x

--- opt/pi-ager/pi_ager_cl_sensor.py
+++ opt/pi-ager/pi_ager_cl_sensor.py
@@ -68,7 +68,7 @@ class cl_main_sensor(cl_ab_temp_sensor, cl_ab_humitity_sensor):
         self.o_sensor_type = o_sensor_type
         
     def get_sensor_type_ui(self):
-        return( self.get_sensor_type_ui() )
+        return( self.o_sensor_type.get_sensor_type_ui() )
     
     def get_dewpoint(self):
         pass
