7a72c3fe02dd5fd26e5e73001571ebc40abf68e2
DerBurgermeister <DerBurgermeister@pi-ager.org>
2019-10-22 09:52:26 +0200

    sht3x

--- opt/pi-ager/pi_ager_sensor_sht3x.py
+++ opt/pi-ager/pi_ager_sensor_sht3x.py
@@ -13,12 +13,16 @@ __status__ = "Production"
 
 #from abc import ABC, abstractmethod
 import inspect
+import pi_ager_logging
 
 from pi_ager_cl_sensor_type import cl_fact_main_sensor_type
 from pi_ager_cl_i2c_bus import cl_fact_i2c_bus_logic
 from pi_ager_cl_i2c_sensor_sht import cl_fact_i2c_sensor_sht
 from pi_ager_cx_exception import *
 from pi_ager_cl_ab_sensor import cl_ab_temp_sensor, cl_ab_humitity_sensor
+
+global logger
+logger = pi_ager_logging.create_logger(__name__) 
 
 class cl_main_sensor_sht3x(cl_ab_temp_sensor, cl_ab_humitity_sensor):
     
