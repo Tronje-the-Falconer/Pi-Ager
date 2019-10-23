efc523b7b3d013349fe5ce44351765cc626effae
DerBurgermeister <DerBurgermeister@pi-ager.org>
2019-10-23 09:17:50 +0200

    sht3x

--- opt/pi-ager/pi_ager_cl_i2c_sensor_sht.py
+++ opt/pi-ager/pi_ager_cl_i2c_sensor_sht.py
@@ -68,7 +68,7 @@ class cl_i2c_sensor_sht(ABC):
         address = 0x44
         msb_data = 0x24
         lsb_data = 0x00
-        self.o_i2c_bus.write_byte_data(address, msb_data, lsb_data)
+        self._i2c_bus.write_byte_data(address, msb_data, lsb_data)
         time.sleep(0.01) #This is so the sensor has tme to preform the mesurement and write its registers before you read it
         
         
