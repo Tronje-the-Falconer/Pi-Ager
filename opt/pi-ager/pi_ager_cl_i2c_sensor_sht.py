85b884792c7fa3abca39f5319ac44480629d0c13
DerBurgermeister <DerBurgermeister@pi-ager.org>
2019-10-23 13:37:43 +0200

    8-Bit CRC check for i2c data frames for SHT sensors

--- opt/pi-ager/pi_ager_cl_i2c_sensor_sht.py
+++ opt/pi-ager/pi_ager_cl_i2c_sensor_sht.py
@@ -56,7 +56,8 @@ class cl_i2c_sensor_sht(ABC):
         crc = 0xFF
 
         # calculates 8-Bit checksum with given polynomial
-        for byteCtr in [ord(x) for x in struct.pack(">H", value)]:
+        #for byteCtr in [ord(x) for x in struct.pack(">H", value)]:
+        for byteCtr in struct.pack(">H", value):
             crc ^= byteCtr
             for bit in range(8, 0, -1):
                 if crc & 0x80:
@@ -84,23 +85,26 @@ class cl_i2c_sensor_sht(ABC):
         address = 0x44
         self.data0 = self._i2c_bus.read_i2c_block_data(address, 0x00, 8)
         self.t_val = (self.data0[0]<<8) + self.data0[1] #convert the data
-        #t_crc_calc = self._calculate_checksum(t_val)
-        self.t_crc = self.data0[2]
-        
         self.h_val = (self.data0[3] <<8) + self.data0[4]     # Convert the data
-        #h_crc_calc = _self._calculate_checksum(h_val)
-        self.h_crc = self.data0[5]
         
+        "CRC Values"
+        t_crc_calc = self._calculate_checksum(self.t_val)
+        h_crc_calc = self._calculate_checksum(self.h_val)
+        
+        t_crc = self.data0[2]
+        h_crc = self.data0[5]
+
         localtime = time.asctime( time.localtime(time.time()) )
-        #if hex(t_crc_calc) != hex(t_crc):
-        if 1 != 1:
+        if hex(t_crc_calc) != hex(t_crc):
+        #if 1 != 1:
             logger.debug("Local current time :", localtime)    
             logger.debug("Temperature CRC calc is : %x " %t_crc_calc)
             logger.debug("Temperature CRC real is : %x " %t_crc) 
+            logger.error("CRC ")
             raise cx_i2c_sht_temperature_crc_error
         
-        #if hex(h_crc_calc) != hex(h_crc):
-        if 1 != 1:
+        if hex(h_crc_calc) != hex(h_crc):
+        #if 1 != 1:
             logger.debug("Local current time :", localtime)    
             logger.debug("Humidity CRC calc is : %x " %h_crc_calc)
             logger.debug("Humidity CRC real is : %x " %h_crc) 
