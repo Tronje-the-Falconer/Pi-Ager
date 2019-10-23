9bf83db4e28cb96c441b6155c7f17b871d377f0d
DerBurgermeister <DerBurgermeister@pi-ager.org>
2019-10-23 08:51:09 +0200

    sht3x

--- opt/pi-ager/pi_ager_loop.py
+++ opt/pi-ager/pi_ager_loop.py
@@ -23,6 +23,7 @@ import pi_ager_organization
 import pi_ager_cx_exception
 from time import ctime as convert
 from pi_ager_cl_alarm import cl_fact_logic_alarm
+from pi_ager_cx_exception import *
 from pi_ager_cl_messenger import cl_fact_logic_messenger
 from pi_ager_cl_sensor_type import cl_fact_main_sensor_type
 from pi_ager_cl_sensor import cl_fact_main_sensor
