cafc197c9625bcf91c5f3e617e4af8aef37004d6
DerBurgermeister <DerBurgermeister@pi-ager.org>
2019-10-24 07:53:56 +0200

    Sensor logic
    Use abstract classes for sensor
    new file for sensor factory class due to circular import

--- opt/pi-ager/pi_ager_cl_sensor.py
+++ opt/pi-ager/pi_ager_cl_sensor.py
@@ -15,10 +15,9 @@ from abc import ABC, abstractmethod
 import inspect
 
 from pi_ager_cl_sensor_type import cl_fact_main_sensor_type
-#from pi_ager_cl_i2c_bus import cl_fact_i2c_bus_logic
+
 from pi_ager_cx_exception import *
-from pi_ager_sensor_sht3x import cl_fact_sensor_sht3x
-#from pi_ager_sensor_sht75 import *
+from pi_ager_cl_sensor_fact import *
 from pi_ager_cl_ab_sensor import cl_ab_temp_sensor, cl_ab_humidity_sensor
         
     
@@ -66,39 +65,5 @@ class th_main_sensor():
         return(self._type)
 
     
-class cl_fact_main_sensor:
-    fact_main_sensor_type = cl_fact_main_sensor_type()
-#    Only a singleton instance for main_sensor
-    __o_sensor_type = fact_main_sensor_type.get_instance()
-    __o_instance = None
-    @classmethod        
-    def get_instance(self):
-        if cl_fact_main_sensor.__o_instance is not None :
-            return(cl_fact_main_sensor.__o_instance)
-        try:
-            if   cl_fact_main_sensor.__o_sensor_type._get_type_ui( ) == 'SHT75':
-#                cl_fact_main_sensor.__o_instance = self.get_instance_sensor_sht75()
-                pass
-            elif cl_fact_main_sensor.__o_sensor_type._get_type_ui( ) == 'SHT3x':
-                cl_fact_main_sensor.__o_instance = cl_fact_sensor_sht3x.get_instance()
-            elif cl_fact_main_sensor.__o_sensor_type._get_type_ui( ) == 'SHT85':
-#                cl_fact_main_sensor.__o_instance = self.get_instance_sensor_sht85()
-                pass   
-            elif cl_fact_main_sensor.__o_sensor_type._get_type_ui( ) == 'DHT22':
-#                cl_fact_main_sensor.__o_instance = self.get_instance_sensor_dht22()
-                pass
-            elif cl_fact_main_sensor.__o_sensor_type._get_type_ui( ) == 'DHT11':
-#                cl_fact_main_sensor.__o_instance = self.get_instance_sensor_dht11()
-                pass
-        except Exception as original_error:
-            raise original_error        
-        return(cl_fact_main_sensor.__o_instance)
 
-    @classmethod
-    def set_instance(self, i_instance):
-        cl_fact_main_sensor.__o_instance = i_instance
-              
-    
-    def __init__(self):
-        pass    
 
