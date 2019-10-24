3f0fce32e9c6cc553661568d55c763b736f4a67d
DerBurgermeister <DerBurgermeister@pi-ager.org>
2019-10-24 09:35:23 +0200

    Sensor logic
    Use abstract classes for sensor
    new file for sensor factory class due to circular import

--- opt/pi-ager/pi_ager_loop.py
+++ opt/pi-ager/pi_ager_loop.py
@@ -26,7 +26,7 @@ from pi_ager_cl_alarm import cl_fact_logic_alarm
 from pi_ager_cx_exception import *
 from pi_ager_cl_messenger import cl_fact_logic_messenger
 from pi_ager_cl_sensor_type import cl_fact_main_sensor_type
-from pi_ager_cl_sensor import cl_fact_main_sensor
+from pi_ager_cl_sensor_fact import cl_fact_main_sensor
 
 
 global logger
