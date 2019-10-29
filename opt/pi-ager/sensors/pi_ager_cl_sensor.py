f2ea10d72f7962bcd4078a9ec0d61c02cb0f7759
DerBurgermeister <DerBurgermeister@pi-ager.org>
2019-10-29 12:31:58 +0100

    InfluxDB test

--- opt/pi-ager/sensors/pi_ager_cl_sensor.py
+++ opt/pi-ager/sensors/pi_ager_cl_sensor.py
@@ -79,7 +79,7 @@ class cl_main_sensor(cl_ab_sensor):
     def _write_to_db(self):
         """ Write the sensor data to time series DB"""
         logger.debug(pi_ager_logging.me())
-        current_time = datetime.datetime.today()
+        current_time = datetime.today()
         influx_db = cl_fact_db_influxdb.get_instance()
         json_body = [
             {
