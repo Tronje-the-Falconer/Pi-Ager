dfdceb85122989626f5439e28b8b0706b1bef014
DerBurgermeister <DerBurgermeister@pi-ager.org>
2020-01-16 16:35:30 +0100

    Pi-Ager_i2c_off.conf.on
    Switch for i2c to 1wire and back - reload Web-Page
    Pi-Ager_i2c_off.conf.on
    
    Switch for i2c to 1wire and back - reload Web-Page

--- var/www/modules/write_bus.php
+++ var/www/modules/write_bus.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 // include 'database.php';
 #Bus Werte in Datenbank schreiben
 if(!empty($_POST['change_sensorbus_submit']))
@@ -8,14 +8,14 @@ if(!empty($_POST['change_sensorbus_submit']))
     if ($bus_value == 1){
         write_busvalue(1);
         logger('DEBUG', 'sensorbus saved. changed to 1wire (1)');
-        
+        print "<script language='javascript'>window.location.href='index.php';</script>";
         shell_exec('sudo /var/sudowebscript.sh sensorbus1wire');
         print '<script language="javascript"> alert("'. (_("sensorbus changed to i2c. please unplug and plug in the power supply for the restart after shutdown.")) . " : " . (_("shutdown")) .'"); </script>';
     }
     if ($bus_value == 0){
         write_busvalue(0);
         logger('DEBUG', 'sensorbus saved. changed to i2c (0)');
-        
+        print "<script language='javascript'>window.location.href='index.php';</script>";
         shell_exec('sudo /var/sudowebscript.sh sensorbusi2c');
         print '<script language="javascript"> alert("'. (_("sensorbus changed to 1wire. please unplug and plug in the power supply for the restart after shutdown.")) . " : " . (_("shutdown")) .'"); </script>';
     }
