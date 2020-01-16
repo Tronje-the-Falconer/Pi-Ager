64528f6cb66986033a7906a1003bc82e1a65bd57
DerBurgermeister <DerBurgermeister@pi-ager.org>
2020-01-16 13:37:11 +0100

    Pi-Ager_i2c_off.conf.on
    Switch for i2c to 1wire and back

--- var/sudowebscript.sh
+++ var/sudowebscript.sh
@@ -118,12 +118,12 @@ case "$1" in
         pushd /var/www/ && zip -r /var/www/logs/pi-ager_logfiles.zip ./logs/ && popd
         #zip -r -j /var/www/logs/pi-ager_logfiles.zip /var/www/logs/
     ;;
-    sensorbusi2c) #Sensorbus wurde geaendert
+    sensorbusi2c) #Sensorbus wurde geaendert auf i2c
 ####### hier muss alles hin was vor dem shutdown gemacht werden soll, um auf i2c zu wechseln
 		rm -r /etc/modprobe.d/Pi-Ager_i2c_off.conf
         shutdown -h now
     ;;
-    sensorbus1wire) #Sensorbus wurde geaendert
+    sensorbus1wire) #Sensorbus wurde geaendert auf 1wire
 ####### hier muss alles hin was vor dem shutdown gemacht werden soll, um auf 1wire zu wechseln
 		cp /etc/modprobe.d/Pi-Ager_i2c_off.conf.on /etc/modprobe.d/Pi-Ager_i2c_off.conf
         shutdown -h now
