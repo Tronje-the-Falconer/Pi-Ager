ae61c2136f14e6ee1f927fa97321b36a3e4dd01c
DerBurgermeister <DerBurgermeister@pi-ager.org>
2020-01-17 16:42:00 +0100

    Pi-Ager_i2c_off.conf.on
    Switch for i2c to 1wire and back - reload Web-Page

--- var/www/modules/write_bus.php
+++ var/www/modules/write_bus.php
@@ -1,35 +1,23 @@
 <?php 
 // include 'database.php';
 #Bus Werte in Datenbank schreiben
-if(!empty($_POST['change_sensorbus_submit']))
-{                       // ist das $_POST-Array gesetzt
-    logger('DEBUG', 'button save change_sensorbus pressed');
-    $bus_value = $_POST['bustype_admin'];
-    if ($bus_value == 1){
-        write_busvalue(1);
-        logger('DEBUG', 'sensorbus saved. changed to 1wire (1)');
-        $bus_string = 'sensorbus1wire';
-    }
-    else if ($bus_value == 0){
-        write_busvalue(0);
-        logger('DEBUG', 'sensorbus saved. changed to i2c (0)');
-        $bus_string = 'sensorbusi2c';
-    }
-    else {
-        $bus_string = '';
-    }
-    #print "<script>window.close();</script>";
-    #file_get_contents('./index.php');
-    #header('Location: index.php');
-    if(isset($_POST['message']) && ($_POST['message'] != $_SESSION['message'])) {
-        $command = 'sudo /var/sudowebscript.sh' . $bus_string;
-        logger('INFO', $command);
-        shell_exec($command);
+if(isset($_POST['message']) && ($_POST['message'] != $_SESSION['message'])) {
+    if(!empty($_POST['change_sensorbus_submit']))
+    {                       // ist das $_POST-Array gesetzt
+        logger('DEBUG', 'button save change_sensorbus pressed');
+        $bus_value = $_POST['bustype_admin'];
+        if ($bus_value == 1){
+            write_busvalue(1);
+            logger('DEBUG', 'sensorbus saved. changed to 1wire (1)');
+            shell_exec('sudo /var/sudowebscript.sh sensorbus1wire');
+        }
+        else if ($bus_value == 0){
+            write_busvalue(0);
+            logger('DEBUG', 'sensorbus saved. changed to i2c (0)');
+            shell_exec('sudo /var/sudowebscript.sh sensorbusi2c');
+        }
         $_SESSION['message'] = $_POST['message'];
-        
     }
-    
-    
     echo "<meta http-equiv='refresh' content='0'>";
 }
 ?>
