ce45546b892666443e23b60d3b3c93cd6871a75e
DerBurgermeister <DerBurgermeister@pi-ager.org>
2020-01-17 16:48:24 +0100

    Pi-Ager_i2c_off.conf.on
    Switch for i2c to 1wire and back - reload Web-Page

--- var/www/modules/write_bus.php
+++ var/www/modules/write_bus.php
@@ -1,22 +1,24 @@
-<?php 
+<?php
 // include 'database.php';
 #Bus Werte in Datenbank schreiben
-if(isset($_POST['message']) && ($_POST['message'] != $_SESSION['message'])) {
-    if(!empty($_POST['change_sensorbus_submit']))
-    {                       // ist das $_POST-Array gesetzt
-        logger('DEBUG', 'button save change_sensorbus pressed');
-        $bus_value = $_POST['bustype_admin'];
-        if ($bus_value == 1){
-            write_busvalue(1);
-            logger('DEBUG', 'sensorbus saved. changed to 1wire (1)');
-            shell_exec('sudo /var/sudowebscript.sh sensorbus1wire');
-        }
-        else if ($bus_value == 0){
-            write_busvalue(0);
-            logger('DEBUG', 'sensorbus saved. changed to i2c (0)');
-            shell_exec('sudo /var/sudowebscript.sh sensorbusi2c');
-        }
-        $_SESSION['message'] = $_POST['message'];
+if(!empty($_POST['change_sensorbus_submit']))
+{                       // ist das $_POST-Array gesetzt
+    logger('DEBUG', 'button save change_sensorbus pressed');
+    $bus_value = $_POST['bustype_admin'];
+    if ($bus_value == 1){
+        write_busvalue(1);
+        logger('DEBUG', 'sensorbus saved. changed to 1wire (1)');
+        
+        shell_exec('sudo /var/sudowebscript.sh sensorbus1wire');
+        print '<script language="javascript"> alert("'. (_("sensorbus changed to i2c. please unplug and plug in the power supply for the restart after shutdown.")) . " : " . (_("shutdown")) .'"); </script>';
     }
+    if ($bus_value == 0){
+        write_busvalue(0);
+        logger('DEBUG', 'sensorbus saved. changed to i2c (0)');
+        
+        shell_exec('sudo /var/sudowebscript.sh sensorbusi2c');
+        print '<script language="javascript"> alert("'. (_("sensorbus changed to 1wire. please unplug and plug in the power supply for the restart after shutdown.")) . " : " . (_("shutdown")) .'"); </script>';
+    }
+    echo "<meta http-equiv='refresh' content='0'>";
 }
 ?>
