4a6aaa0a1ea99bcf3ec9394b660d74e160e035ea
DerBurgermeister <DerBurgermeister@pi-ager.org>
2020-01-17 15:17:42 +0100

    Pi-Ager_i2c_off.conf.on
    Switch for i2c to 1wire and back - reload Web-Page

--- var/www/modules/write_bus.php
+++ var/www/modules/write_bus.php
@@ -8,13 +8,13 @@ if(!empty($_POST['change_sensorbus_submit']))
     if ($bus_value == 1){
         write_busvalue(1);
         logger('DEBUG', 'sensorbus saved. changed to 1wire (1)');
-        echo "<script>window.close();</script>";
+        print "<script>window.close();</script>";
         shell_exec('sudo /var/sudowebscript.sh sensorbus1wire');
     }
     if ($bus_value == 0){
         write_busvalue(0);
         logger('DEBUG', 'sensorbus saved. changed to i2c (0)');
-        echo "<script>window.close();</script>";
+        print "<script>window.close();</script>";
         shell_exec('sudo /var/sudowebscript.sh sensorbusi2c');
     }
     echo "<meta http-equiv='refresh' content='0'>";
