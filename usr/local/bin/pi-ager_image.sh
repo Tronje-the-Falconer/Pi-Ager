1823e560709957b4f1ef61b78d91caf93ed4712a
DerBurgermeister <DerBurgermeister@pi-ager.org>
2020-01-20 21:37:53 +0100

    Backup and Image scripts
    hostname error fixed

--- usr/local/bin/pi-ager_image.sh
+++ usr/local/bin/pi-ager_image.sh
@@ -41,7 +41,7 @@ img_old="$1"
 img="PiAger_image.img"
 echo "Coping $img_old to $img"
 
-rsync -a --info=progress2 "$img_old" "$img"
+rsync -a --info=progress2 "./$img_old" "$img"
 read -p "Press enter to continue after copy image"
 
 parted_output=$(parted -ms "$img" unit B print | tail -n 1)
