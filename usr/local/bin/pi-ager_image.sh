24ea4bfdedbb76ddbd0d339e444fc7d47c496df6
DerBurgermeister <DerBurgermeister@pi-ager.org>
2020-01-24 16:48:57 +0100

    Backup and Image scripts
    now with no apt update and upgrade, only remove and with pip update
    mount of boot added /boot
    Command line options added
    Boot partition resized to 250 MB
    Umount /boot
    Error with mounting /boot: Fehler:  Datei »/boot/start.elf« kann nicht
    geöffnet werden: Datei oder Verzeichnis nicht gefunden
    Removed mount /boot and unmount /boot due to /book.bak has the start.elf
    file.
    Copy /boot.bak/ to /boot/ and after update copy /boot/ to /boot.bak/

--- usr/local/bin/pi-ager_image.sh
+++ usr/local/bin/pi-ager_image.sh
@@ -213,12 +213,14 @@ done
 umount "$mountdir"
 if [ $? -ne 0 ]
 then
-  echo "Error unmounting $mountdir. Maybe $mountdir is open. Image is then corrupt."
-  exit 1
+  	echo "Error unmounting $mountdir. Maybe $mountdir is open. Image is then corrupt."
+  	exit 1
 else
-rm -rf $mountdir
-# Shrink image
-pishrink.sh -r ${BACKUP_PFAD}/$img 
-# Backup umbenennen mit Datum
-mv ${BACKUP_PFAD}/PiAger_image.img ${BACKUP_PFAD}/PiAger_image_$(date +%Y-%m-%d-%H:%M:%S).img
+	rm -rf $mountdir
+	# Shrink image
+	pishrink.sh -r ${BACKUP_PFAD}/$img 
+	# Backup umbenennen mit Datum
+	mv ${BACKUP_PFAD}/PiAger_image.img ${BACKUP_PFAD}/PiAger_image_$(date +%Y-%m-%d-%H:%M:%S).img
+	echo "The image ${BACKUP_PFAD}/PiAger_image_$(date +%Y-%m-%d-%H:%M:%S).img was successfully created."
+	
 fi
