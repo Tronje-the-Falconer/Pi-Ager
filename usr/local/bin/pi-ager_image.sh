d44984e7725d8588fbd8e733a61efc2813da91d8
DerBurgermeister <DerBurgermeister@pi-ager.org>
2020-01-29 13:13:46 +0100

    Backup and Image scripts
    Change SQLite settings
    -Set debug levels = INFO
    -Delete some tables
    -zip output file

--- usr/local/bin/pi-ager_image.sh
+++ usr/local/bin/pi-ager_image.sh
@@ -265,8 +265,8 @@ else
 	rm -rf $mountdir/boot
 	rm -rf $mountdir
 	
-	# Shrink image
-	pishrink.sh -r ${BACKUP_PFAD}/$img 
+	# Shrink and zip image
+	pishrink.sh -r -z ${BACKUP_PFAD}/$img 
 	# Backup umbenennen mit Datum
 	mv ${BACKUP_PFAD}/PiAger_image.img ${BACKUP_PFAD}/PiAger_image_$(date +%Y-%m-%d-%H:%M:%S).img
 	echo "The image ${BACKUP_PFAD}/PiAger_image_$(date +%Y-%m-%d-%H:%M:%S).img was successfully created."
