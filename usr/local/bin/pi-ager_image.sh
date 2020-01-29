ed1bfeab19df7b3aca869c9904cbbd5161dc7c5f
DerBurgermeister <DerBurgermeister@pi-ager.org>
2020-01-29 21:10:23 +0100

    Backup and Image scripts
    Change SQLite settings
    -Set debug levels = INFO
    -Delete some tables
    -zip output file

--- usr/local/bin/pi-ager_image.sh
+++ usr/local/bin/pi-ager_image.sh
@@ -264,21 +264,11 @@ then
 else
 	rm -rf $mountdir/boot
 	rm -rf $mountdir
-
-	if [ 1 == 1 ]
-	then
-		# Shrink image
-		pishrink.sh -r ${BACKUP_PFAD}/$img 
-		# Backup umbenennen mit Datum
-		mv ${BACKUP_PFAD}/$img ${BACKUP_PFAD}/PiAger_image_$(date +%Y-%m-%d-%H:%M:%S).img.gz
-	else
-		# Shrink and zip image
-		pishrink.sh -r -z ${BACKUP_PFAD}/$img 
-		#${BACKUP_PFAD}/PiAger_image_$(date +%Y-%m-%d-%H:%M:%S).img.zip.
-		# Backup umbenennen mit Datum
-		mv ${BACKUP_PFAD}/PiAger_image.img.gz ${BACKUP_PFAD}/PiAger_image_$(date +%Y-%m-%d-%H:%M:%S).img.gz
-	fi
-	echo "The image ${BACKUP_PFAD}/PiAger_image_$(date +%Y-%m-%d-%H:%M:%S).img.gz was successfully created."
-
-
+	
+	# Shrink image
+	pishrink.sh -r ${BACKUP_PFAD}/$img 
+	# Backup umbenennen mit Datum
+	mv ${BACKUP_PFAD}/PiAger_image.img ${BACKUP_PFAD}/PiAger_image_$(date +%Y-%m-%d-%H:%M:%S).img
+	echo "The image ${BACKUP_PFAD}/PiAger_image_$(date +%Y-%m-%d-%H:%M:%S).img was successfully created."
+	
 fi
