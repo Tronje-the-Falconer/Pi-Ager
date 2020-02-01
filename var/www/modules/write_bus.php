8516255f338017b8faee239dc5f58852fe73ee13
Tronje-the-Falconer <club@kording-web.de>
2020-02-01 20:42:22 +0100

    Update write_bus.php
    Bugfix shutdown

--- var/www/modules/write_bus.php
+++ var/www/modules/write_bus.php
@@ -18,11 +18,12 @@
             }
             $_SESSION['message'] = $_POST['message'];
         }
+    header("Location: /index.php");
+    die();
     #3 Sekunden anzeigen, dass System heruntergefahren wird
         print '<p id=\'info-message\' style=\'color: #ff0000; font-size: 20px;\'><b>'. (_("system shutdown in 10 seconds")) .'</b><br>' . date("m/d/y h:i:s a") . ' </p>
             <script language="javascript">
-                setTimeout(function(){document.getElementById("info-message").style.display="none"}, 3000)
-                setTimeout(function(){window.location.href= "/index.php";},3000); 
+                setTimeout(function(){document.getElementById("info-message").style.display="none"}, 10000); 
             </script>';
     }
 ?>
