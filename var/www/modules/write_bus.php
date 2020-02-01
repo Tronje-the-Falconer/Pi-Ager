5591e077feecb3c97f27e7867c98b47f20c89900
Tronje-the-Falconer <club@kording-web.de>
2020-02-01 21:08:31 +0100

    Update write_bus.php

--- var/www/modules/write_bus.php
+++ var/www/modules/write_bus.php
@@ -16,14 +16,14 @@
                 logger('DEBUG', 'sensorbus saved. changed to i2c (0)');
                 shell_exec('sudo /var/sudowebscript.sh sensorbusi2c');
             }
-            $_SESSION['message'] = $_POST['message'];
-        }
-    header("Location: /index.php");
-    die();
+            $_SESSION['message'] = $_POST['message'];        
+        header("Location: /index.php");
+        die();
     #3 Sekunden anzeigen, dass System heruntergefahren wird
         print '<p id=\'info-message\' style=\'color: #ff0000; font-size: 20px;\'><b>'. (_("system shutdown in 10 seconds")) .'</b><br>' . date("m/d/y h:i:s a") . ' </p>
             <script language="javascript">
                 setTimeout(function(){document.getElementById("info-message").style.display="none"}, 10000); 
             </script>';
+        }
     }
 ?>
