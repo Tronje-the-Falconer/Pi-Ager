78cb977ef22316ed88d825c63bea3d298211a062
Tronje-the-Falconer <club@kording-web.de>
2020-02-01 21:07:33 +0100

    Update system_reboot.php

--- var/www/modules/system_reboot.php
+++ var/www/modules/system_reboot.php
@@ -4,12 +4,12 @@
         logger('DEBUG', 'button reboot pressed');
         $date = date('d.m.Y H:i:s');
         shell_exec('sudo /var/sudowebscript.sh reboot');
-    header("Location: /index.php");
-    die();
+        header("Location: /index.php");
+        die();
 #3 Sekunden anzeigen, dass System neugestartet wird
-    print '<p id=\'info-message\' style=\'color: #ff0000; font-size: 20px;\'><b>'. (_("system shutdown in 10 seconds")) .'</b><br>' . $date . ' </p>
-        <script language="javascript">
-            setTimeout(function(){document.getElementById("info-message").style.display="none"}, 10000)
-        </script>';
+        print '<p id=\'info-message\' style=\'color: #ff0000; font-size: 20px;\'><b>'. (_("system shutdown in 10 seconds")) .'</b><br>' . $date . ' </p>
+            <script language="javascript">
+                setTimeout(function(){document.getElementById("info-message").style.display="none"}, 10000)
+            </script>';
     }
 ?>
