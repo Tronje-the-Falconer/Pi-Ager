8c8901645b80d64a3828e5bd5c5ed054b608c113
Tronje-the-Falconer <club@kording-web.de>
2020-02-01 20:39:56 +0100

    Update system_shutdown.php
    Bugfix Shutdown system

--- var/www/modules/system_shutdown.php
+++ var/www/modules/system_shutdown.php
@@ -4,11 +4,12 @@
         logger('DEBUG', 'button shutdown pressed');
         $date = date('d.m.Y H:i:s');
         shell_exec('sudo /var/sudowebscript.sh shutdown');
+    header("Location: /index.php");
+    die();
 #3 Sekunden anzeigen, dass System heruntergefahren wird
         print '<p id="info-message" style="color: #ff0000; font-size: 20px;"><b>'.  (_("system shutdown in 10 seconds")) .'</b><br>' . $date . ' </p>
             <script language="javascript">
-                setTimeout(function(){document.getElementById("info-message").style.display="none"}, 3000)
-                setTimeout(function(){window.location.href= "/index.php";},3000); 
+                setTimeout(function(){document.getElementById("info-message").style.display="none"}, 10000);
             </script>';
     }
 ?>
