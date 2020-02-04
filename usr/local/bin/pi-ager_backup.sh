59d4601e53d5a424034cd6d3e80181bf92c1d68f
DerBurgermeister <DerBurgermeister@pi-ager.org>
2020-02-04 21:41:38 +0100

    Backup script
    Starting the services that were already running

--- usr/local/bin/pi-ager_backup.sh
+++ usr/local/bin/pi-ager_backup.sh
@@ -163,15 +163,15 @@ dd if=/dev/mmcblk0 of=${BACKUP_PFAD}/${BACKUP_NAME}.img bs=1M status=progress
 # Starte Dienste nach Backup
 echo "Starte schreibende Dienste wieder!"
 #${DIENSTE_START_STOP} start
-if [ $PI_AGER_MAIN_ACTIVE == 1 ] then
+if [ $PI_AGER_MAIN_ACTIVE == 1 ]; then
   systemctl start pi-ager_main
 fi
 
-if [ $PI_AGER_SCALE_ACTIVE == 1 ] then
+if [ $PI_AGER_SCALE_ACTIVE == 1 ]; then
   systemctl start pi-ager_scale
 fi
 
-if [ $PI_AGER_AGINGTABLE_ACTIVE == 1 ] then
+if [ $PI_AGER_AGINGTABLE_ACTIVE == 1 ]; then
   systemctl start pi-ager_agingtable
 fi
 
