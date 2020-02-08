5053ac8a817293714ad5af04c5b868580ceff41a
DerBurgermeister <DerBurgermeister@pi-ager.org>
2020-02-08 13:39:46 +0100

    Messenger, Pushover and Telegram
    cl_ab_database_config

--- opt/pi-ager/messenger/pi_ager_cl_crypt.py
+++ opt/pi-ager/messenger/pi_ager_cl_crypt.py
@@ -42,7 +42,7 @@ class cl_help_crypt:
             raise cx_direct_call("Please use factory class")
         
         self.cx_error  = cx_error
-        self.pi_serial = self.getserial()
+        self.pi_serial = self.getserial() + self.getserial()
         self.cipher_suite = Fernet(self.pi_serial)
         
     def getserial(self):
