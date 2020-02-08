7cde332575c8c362da61fa9e865b097b66435efb
DerBurgermeister <DerBurgermeister@pi-ager.org>
2020-02-08 14:39:46 +0100

    Messenger, Pushover and Telegram
    cl_ab_database_config

--- opt/pi-ager/messenger/pi_ager_cl_crypt.py
+++ opt/pi-ager/messenger/pi_ager_cl_crypt.py
@@ -45,7 +45,7 @@ class cl_help_crypt:
         
         self.pi_serial = self.get_serial()
         
-    def get_serial():
+    def get_serial(self):
       # Extract serial from cpuinfo file
       cpuserial = "0000000000000000"
       try:
