dce4e50b2d0a9b921cf9f788c4b48fea37893381
DerBurgermeister <DerBurgermeister@pi-ager.org>
2020-02-10 16:23:07 +0100

    system_reboot, shutdown and bus-change

--- var/www/modules/system_shutdown.php
+++ var/www/modules/system_shutdown.php
@@ -2,14 +2,16 @@
     #System herunterfahren
     if (isset ($_POST['shutdown'])){
         logger('DEBUG', 'button shutdown pressed');
+        unset($_POST['shutdown']);
         $date = date('d.m.Y H:i:s');
-        shell_exec('sudo /var/sudowebscript.sh shutdown');
-        header("Location: /index.php");
+        shell_exec('sudo /var/sudowebscript.sh shutdown > /dev/null 2>&1 &');
+#        header("Location: /index.php");
+        print '<p id=\'info-message\' style=\'color: #ff0000; font-size: 20px;\'><b>'. (_("system shutdown in 10 seconds")) .'</b><br>' . date("m/d/y h:i:s a") . ' </p>';        
         die();
 #3 Sekunden anzeigen, dass System heruntergefahren wird
-        print '<p id="info-message" style="color: #ff0000; font-size: 20px;"><b>'.  (_("system shutdown in 10 seconds")) .'</b><br>' . $date . ' </p>
-            <script language="javascript">
-                setTimeout(function(){document.getElementById("info-message").style.display="none"}, 10000);
-            </script>';
+#        print '<p id="info-message" style="color: #ff0000; font-size: 20px;"><b>'.  (_("system shutdown in 10 seconds")) .'</b><br>' . $date . ' </p>
+#            <script language="javascript">
+#                setTimeout(function(){document.getElementById("info-message").style.display="none"}, 10000);
+#            </script>';
     }
 ?>
