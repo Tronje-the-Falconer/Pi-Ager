4aba99c095c78f5509f95a1cf82769e270cb8617
DerBurgermeister <DerBurgermeister@pi-ager.org>
2020-02-16 09:30:08 +0100

    scale_loop.py performace improvement

--- opt/pi-ager/scale_loop.py
+++ opt/pi-ager/scale_loop.py
@@ -10,76 +10,87 @@ from main.pi_ager_cl_logger import cl_fact_logger
 
 def tara_scale(scale, tara_key, data_table, calibrate_key, offset, settings_table):
     cl_fact_logger.get_instance().debug('performing tara')
-    #scale.reset()
-    #scale.tare()
-    
-    pi_ager_database.update_value_in_table(settings_table, pi_ager_names.offset_scale_key, 0) # set offset to zero to get right offset value
-    offset = 0
-    
-    scale.setSamples(int(pi_ager_database.get_table_value(pi_ager_names.config_settings_table, pi_ager_names.samples_refunit_tara_key)))
-    scale.setSpikes(int(pi_ager_database.get_table_value(pi_ager_names.config_settings_table, pi_ager_names.spikes_refunit_tara_key)))
-    
-    clear_history = scale.getWeight() # delete values out of history
-    
-    tara_measuring_endtime = pi_ager_database.get_current_time() + 1
-    pi_ager_database.update_value_in_table(pi_ager_names.current_values_table, tara_key, 2)
-    
-    newoffset = scale_measures(scale, tara_measuring_endtime, data_table, 1, tara_key, calibrate_key, offset, settings_table)
-    pi_ager_database.update_value_in_table(settings_table, pi_ager_names.offset_scale_key, newoffset)
-    pi_ager_database.write_stop_in_database(tara_key)
-    
-    scale.setSamples(int(pi_ager_database.get_table_value(settings_table, pi_ager_names.samples_key)))
-    scale.setSpikes(int(pi_ager_database.get_table_value(settings_table, pi_ager_names.spikes_key)))
-    
-    cl_fact_logger.get_instance().debug('tara performed - runnig control-measurement')
-    # im Anschluss eine Kontrollmessung machen
-    scale_measures(scale, tara_measuring_endtime, data_table, 1, tara_key, calibrate_key, newoffset, settings_table)
+    try:
+        #scale.reset()
+        #scale.tare()
+        
+        pi_ager_database.update_value_in_table(settings_table, pi_ager_names.offset_scale_key, 0) # set offset to zero to get right offset value
+        offset = 0
+        
+        scale.setSamples(int(pi_ager_database.get_table_value(pi_ager_names.config_settings_table, pi_ager_names.samples_refunit_tara_key)))
+        scale.setSpikes(int(pi_ager_database.get_table_value(pi_ager_names.config_settings_table, pi_ager_names.spikes_refunit_tara_key)))
+        
+        clear_history = scale.getWeight() # delete values out of history
+        
+        tara_measuring_endtime = pi_ager_database.get_current_time() + 1
+        pi_ager_database.update_value_in_table(pi_ager_names.current_values_table, tara_key, 2)
+        
+        newoffset = scale_measures(scale, tara_measuring_endtime, data_table, 1, tara_key, calibrate_key, offset, settings_table)
+        pi_ager_database.update_value_in_table(settings_table, pi_ager_names.offset_scale_key, newoffset)
+        pi_ager_database.write_stop_in_database(tara_key)
+        
+        scale.setSamples(int(pi_ager_database.get_table_value(settings_table, pi_ager_names.samples_key)))
+        scale.setSpikes(int(pi_ager_database.get_table_value(settings_table, pi_ager_names.spikes_key)))
+        
+        cl_fact_logger.get_instance().debug('tara performed - runnig control-measurement')
+        # im Anschluss eine Kontrollmessung machen
+        scale_measures(scale, tara_measuring_endtime, data_table, 1, tara_key, calibrate_key, newoffset, settings_table)
+    except Exception as cx_error:
+            cl_fact_logic_messenger().get_instance().handle_exception(cx_error)
 
 def scale_measures(scale, scale_measuring_endtime, data_table, saving_period, tara_key, calibrate_scale_key, offset, settings_table):
     cl_fact_logger.get_instance().debug('scale_measures()')
-    measure_start_time = pi_ager_database.get_current_time()
-    
-    save_time = 0
-    current_time = measure_start_time
-    while current_time <= int(scale_measuring_endtime):
-        calibrate_scale = pi_ager_database.get_table_value(pi_ager_names.current_values_table, calibrate_scale_key)
-        if calibrate_scale != 0:
-            scale_measuring_endtime = current_time
-        status_tara_scale = pi_ager_database.get_table_value(pi_ager_names.current_values_table, tara_key)
-        if status_tara_scale == 1:
-            tara_scale(scale, tara_key, data_table, calibrate_scale_key, offset, settings_table)
-        value = scale.getMeasure()
-        value = value - offset
-        if status_tara_scale == 2:
-            cl_fact_logger.get_instance().debug('tara measurement performed')
-            return value
-        formated_value = round(value, 3)
-        if (current_time - measure_start_time) % saving_period == 0 and current_time != save_time:      # speichern je nach datenbankeintrag fuer saving_period
-            save_time = current_time
-            pi_ager_database.write_scale(data_table,value)
-            cl_fact_logger.get_instance().debug('scale-value saved in database ' + time.strftime('%H:%M:%S', time.localtime()))
-        current_time = pi_ager_database.get_current_time()
-    cl_fact_logger.get_instance().debug('measurement performed')
+    try:
+        measure_start_time = pi_ager_database.get_current_time()
+        
+        save_time = 0
+        current_time = measure_start_time
+        while current_time <= int(scale_measuring_endtime):
+            calibrate_scale = pi_ager_database.get_table_value(pi_ager_names.current_values_table, calibrate_scale_key)
+            if calibrate_scale != 0:
+                scale_measuring_endtime = current_time
+            status_tara_scale = pi_ager_database.get_table_value(pi_ager_names.current_values_table, tara_key)
+            if status_tara_scale == 1:
+                tara_scale(scale, tara_key, data_table, calibrate_scale_key, offset, settings_table)
+            value = scale.getMeasure()
+            value = value - offset
+            if status_tara_scale == 2:
+                cl_fact_logger.get_instance().debug('tara measurement performed')
+                return value
+            formated_value = round(value, 3)
+            if (current_time - measure_start_time) % saving_period == 0 and current_time != save_time:      # speichern je nach datenbankeintrag fuer saving_period
+                save_time = current_time
+                pi_ager_database.write_scale(data_table,value)
+                cl_fact_logger.get_instance().debug('scale-value saved in database ' + time.strftime('%H:%M:%S', time.localtime()))
+            current_time = pi_ager_database.get_current_time()
+        cl_fact_logger.get_instance().debug('measurement performed')
+    except Exception as cx_error:
+            cl_fact_logic_messenger().get_instance().handle_exception(cx_error)
 
 def get_scale_settings(scale_setting_rows):
-   
-    scale_settings = {}
-    for scale_setting_row in scale_setting_rows:
-        scale_settings[scale_setting_row[pi_ager_names.key_field]] = scale_setting_row[pi_ager_names.value_field]
+    try:
+        scale_settings = {}
+        for scale_setting_row in scale_setting_rows:
+            scale_settings[scale_setting_row[pi_ager_names.key_field]] = scale_setting_row[pi_ager_names.value_field]
+    except Exception as cx_error:
+            cl_fact_logic_messenger().get_instance().handle_exception(cx_error)
     return scale_settings
     
 def get_first_calibrate_measure(scale, scale_settings_table, calibrate_scale_key):
-    # scale.setReferenceUnit(1)
-    scale.setReferenceUnit(pi_ager_database.get_table_value(scale_settings_table, pi_ager_names.referenceunit_key))
-    scale.setSamples(int(pi_ager_database.get_table_value(pi_ager_names.config_settings_table, pi_ager_names.samples_refunit_tara_key)))
-    scale.setSpikes(int(pi_ager_database.get_table_value(pi_ager_names.config_settings_table, pi_ager_names.spikes_refunit_tara_key)))
-    # scale.reset()
-    # scale.tare()
-    clear_history = scale.getWeight()
-    calibrate_value_before_weight = scale.getMeasure()
-    pi_ager_database.write_current_value(calibrate_scale_key,2)
-    scale.setSamples(int(pi_ager_database.get_table_value(scale_settings_table, pi_ager_names.samples_key)))
-    scale.setSpikes(int(pi_ager_database.get_table_value(scale_settings_table, pi_ager_names.spikes_key)))
+    try:
+        # scale.setReferenceUnit(1)
+        scale.setReferenceUnit(pi_ager_database.get_table_value(scale_settings_table, pi_ager_names.referenceunit_key))
+        scale.setSamples(int(pi_ager_database.get_table_value(pi_ager_names.config_settings_table, pi_ager_names.samples_refunit_tara_key)))
+        scale.setSpikes(int(pi_ager_database.get_table_value(pi_ager_names.config_settings_table, pi_ager_names.spikes_refunit_tara_key)))
+        # scale.reset()
+        # scale.tare()
+        clear_history = scale.getWeight()
+        calibrate_value_before_weight = scale.getMeasure()
+        pi_ager_database.write_current_value(calibrate_scale_key,2)
+        scale.setSamples(int(pi_ager_database.get_table_value(scale_settings_table, pi_ager_names.samples_key)))
+        scale.setSpikes(int(pi_ager_database.get_table_value(scale_settings_table, pi_ager_names.spikes_key)))
+    except Exception as cx_error:
+            cl_fact_logic_messenger().get_instance().handle_exception(cx_error)
     return calibrate_value_before_weight
     
 def calculate_reference_unit(scale, calibrate_scale_key, scale_settings_table, calibrate_value_first_measure):
@@ -196,7 +207,7 @@ def doScaleLoop():
                     scale2_measuring_endtime = pi_ager_database.get_current_time() + scale2_measuring_duration
                     scale_measures(scale2, scale2_measuring_endtime, pi_ager_names.data_scale2_table, saving_period_scale2, pi_ager_names.status_tara_scale2_key, pi_ager_names.calibrate_scale2_key, offset_scale2, pi_ager_names.settings_scale2_table)
         except Exception as cx_error:
-            cl_fact_logic_messenger().get_instance(cx_error).send()
+            cl_fact_logic_messenger().get_instance().handle_exception(cx_error)
             pass    
             
         time.sleep(2)
