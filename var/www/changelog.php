5efca84295ed9e3a4f43e8434633f2f79738b49c
DerBurgermeister <DerBurgermeister@pi-ager.org>
2020-02-23 10:56:26 +0100

    Changelog style

--- var/www/changelog.php
+++ var/www/changelog.php
@@ -38,7 +38,7 @@
                                     <table class="miniature_writing">
                                         <tr>
                                             <td>
-                                                <div style="margin-left: 2em; text-indent: 0em; tab-size:5; -moz-tab-size:5; -ms-tab-size:5;white-space:pre-wrap;">
+                                                <div style="display: block; tab-size:5; -moz-tab-size:5; -ms-tab-size:5;white-space:pre-wrap;">
                                                     <?php 
                                                         $f = file('changelog.txt');
                                                         foreach($f as $file) {
