897e0766b1b9ef3fb09b87e0261fb1dd0e45a612
phylax2020 <severin.hoffmann@t-online.de>
2020-03-06 20:16:56 +0100

    After sensor change index.php is shown

--- var/www/modules/write_bus.php
+++ var/www/modules/write_bus.php
@@ -20,7 +20,8 @@
             logger('DEBUG', 'sensorbus saved. changed to i2c (0)');
             shell_exec('sudo /var/sudowebscript.sh sensorbusi2c > /dev/null 2>&1 &');
         }
-        print '<p id=\'info-message\' style=\'color: #ff0000; font-size: 20px;\'><b>'. (_("system shutdown in 10 seconds")) .'</b><br>' . date("m/d/y h:i:s a") . ' </p>';        
+        print '<p id=\'info-message\' style=\'color: #ff0000; font-size: 20px;\'><b>'. (_("system shutdown in 10 seconds")) .'</b><br>' . date("m/d/y h:i:s a") . ' </p>';
+        header("Location: /index.php");
         die();
     #10 Sekunden anzeigen, dass System heruntergefahren wird
 #        print '<p id=\'info-message\' style=\'color: #ff0000; font-size: 20px;\'><b>'. (_("system shutdown in 10 seconds")) .'</b><br>' . date("m/d/y h:i:s a") . ' </p>
