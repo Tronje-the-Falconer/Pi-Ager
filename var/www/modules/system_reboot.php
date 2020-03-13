37256e0b8950b8ffc3268c1debd6571f0d9f14f7
phylax2020 <severin.hoffmann@t-online.de>
2020-03-13 10:10:44 +0100

    System shutdown/reboot redirect to index.php showing message

--- var/www/modules/system_reboot.php
+++ var/www/modules/system_reboot.php
@@ -5,8 +5,8 @@
         unset($_POST['reboot']);
         $date = date('d.m.Y H:i:s');
         shell_exec('sudo /var/sudowebscript.sh reboot > /dev/null 2>&1 &');
-#        header("Location: /index.php");
-        print '<p id=\'info-message\' style=\'color: #ff0000; font-size: 20px;\'><b>'. (_("system reboot in 10 seconds")) .'</b><br>' . date("m/d/y h:i:s a") . ' </p>';      
+        header("Location: /reboot.php");
+#        print '<p id=\'info-message\' style=\'color: #ff0000; font-size: 20px;\'><b>'. (_("system reboot in 10 seconds")) .'</b><br>' . date("m/d/y h:i:s a") . ' </p>';      
         die();
 #3 Sekunden anzeigen, dass System neugestartet wird
 #        print '<p id=\'info-message\' style=\'color: #ff0000; font-size: 20px;\'><b>'. (_("system shutdown in 10 seconds")) .'</b><br>' . $date . ' </p>
