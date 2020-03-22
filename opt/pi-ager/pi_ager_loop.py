db44d5d6a30459b5df9cf3ef25de0f8be5089138
DerBurgermeister <DerBurgermeister@pi-ager.org>
2020-03-22 07:13:56 +0100

    Problems with exhaust fan

--- opt/pi-ager/pi_ager_loop.py
+++ opt/pi-ager/pi_ager_loop.py
@@ -776,8 +776,8 @@ def doMainLoop():
                     gpio.output(pi_ager_gpio_config.gpio_dehumidifier, pi_ager_names.relay_off)
                 
                 # Schalten des (Abluft-)Luftaustausch-Ventilator
-                cl_fact_logger.get_instance().debug(_('status_exhaust_air =') + ': ' + str(status_exhaust_air))
-                cl_fact_logger.get_instance().debug(_('status_exhaust_fan =') + ': ' + str(status_exhaust_air))
+                cl_fact_logger.get_instance().debug(_('status_exhaust_air =') + ': ' + str(status_exhaust_air + '%s' %(value or '')))
+                cl_fact_logger.get_instance().debug(_('status_exhaust_fan =') + ': ' + str(status_exhaust_air + '%s' %(value or '')))
                 
                 #cl_fact_logger.get_instance().debug('status_exhaust_air = ' + status_exhaust_air)
                 #cl_fact_logger.get_instance().debug('status_exhaust_fan = ' + status_exhaust_air)
