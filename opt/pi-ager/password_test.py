ffbf89e2ad2353d86423a8dfd412601eabdb3074
DerBurgermeister <DerBurgermeister@pi-ager.org>
2020-05-24 18:26:58 +0200

    Corrected password generation

---
+++ opt/pi-ager/password_test.py
@@ -0,0 +1,16 @@
+from main.pi_ager_cx_exception import *
+from main.pi_ager_cl_logger import cl_fact_logger
+
+from messenger.pi_ager_cl_crypt import cl_fact_help_crypt
+from Demos.security.sspi.validate_password import password
+password = ''
+print("Password             = " + password)
+crypt = cl_fact_help_crypt.get_instance()
+print("Encrypt")
+encrypted_secret = crypt.encrypt(password).decode("utf-8") 
+print("Secret = " + encrypted_secret)
+print("Decrypt")
+
+decrypted_secret = crypt.decrypt(encrypted_secret)
+print(decrypted_secret)
+
