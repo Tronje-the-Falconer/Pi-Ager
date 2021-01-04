c40d27f9df077aa023f87eef50db33b7835612cd
DerBurgermeister <DerBurgermeister@pi-ager.org>
2021-01-04 14:06:40 +0100

    Backup und Changelog

--- var/www/modules/backup_manual.php
+++ var/www/modules/backup_manual.php
@@ -3,11 +3,6 @@
     if (isset ($_POST['manual_backup'])){
         logger('DEBUG', 'button manual backup pressed');
         unset($_POST['manual_backup']);
-        if ('sudo /var/sudowebscript.sh grepagingtable' == NULL){
-            shell_exec('sudo /var/sudowebscript.sh backup > /dev/null 2>&1 &');
-        }
-        else{
-          echo "Agingtable is active - no Backup possible!";  
-        }
+        shell_exec('sudo /var/sudowebscript.sh backup > /dev/null 2>&1 &');
     }
 ?>
