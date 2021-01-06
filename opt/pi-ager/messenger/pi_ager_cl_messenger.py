c02423a5ef51192b1cc63637bb7343b8a48462fb
DerBurgermeister <DerBurgermeister@pi-ager.org>
2021-01-06 17:58:32 +0100

    Messenger for Events

--- opt/pi-ager/messenger/pi_ager_cl_messenger.py
+++ opt/pi-ager/messenger/pi_ager_cl_messenger.py
@@ -128,6 +128,7 @@ class cl_logic_messenger: #Sollte logic heissen und dann dec, db und helper...
     def handle_event(self, event, info_text=None):
         """
         Handle event to create alarm or email or telegram or pushover ... class
+        If the second parameter info_text is empty, the value is taken from the field envent_text in table config_messenger_event 
         """
         cl_fact_logger.get_instance().debug(cl_fact_logger.get_instance().me())
         cl_fact_logger.get_instance().info("Event raised: " + event )
