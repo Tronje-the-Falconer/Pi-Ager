b6529c3fc520e7fdf16d8cee859e31a782e0d0b0
DerBurgermeister <DerBurgermeister@pi-ager.org>
2021-01-06 18:04:46 +0100

    Messenger for Events

--- opt/pi-ager/messenger/pi_ager_cl_messenger.py
+++ opt/pi-ager/messenger/pi_ager_cl_messenger.py
@@ -131,7 +131,7 @@ class cl_logic_messenger: #Sollte logic heissen und dann dec, db und helper...
         If the second parameter info_text is empty, the value is taken from the field envent_text in table config_messenger_event 
         """
         cl_fact_logger.get_instance().debug(cl_fact_logger.get_instance().me())
-        cl_fact_logger.get_instance().info("Event raised: " + event + ' with info text:'+ info_text )
+        cl_fact_logger.get_instance().info("Event raised: " + event + ' with info text:'+ str(info_text) )
         
         self.event        = event
      
