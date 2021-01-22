fa3bf5ca17853aaf0c4b6763787b4a5e92506424
DerBurgermeister <DerBurgermeister@pi-ager.org>
2021-01-22 16:24:47 +0100

    Sensor fact issues

--- opt/pi-ager/sensors/pi_ager_cl_sensor_sht.py
+++ opt/pi-ager/sensors/pi_ager_cl_sensor_sht.py
@@ -63,7 +63,9 @@ class cl_sensor_sht(cl_sensor, ABC):
         self.o_sensor_type = i_sensor_type
         super().__init__(self.o_sensor_type)
         self.o_active_sensor= i_active_sensor
-
+        self._i2c_bus = cl_fact_i2c_bus_logic.get_instance().get_i2c_bus()
+        cl_fact_logger.get_instance().debug(self._i2c_bus)
+        self._i2c_sensor = cl_fact_i2c_sensor_sht.get_instance(self.o_active_sensor, self._i2c_bus, self.o_address)
 
 
  
@@ -93,9 +95,7 @@ class cl_sensor_sht(cl_sensor, ABC):
         cl_fact_logger.get_instance().debug(cl_fact_logger.get_instance().me())
                      
         try:
-            self._i2c_bus = cl_fact_i2c_bus_logic.get_instance().get_i2c_bus()
-            cl_fact_logger.get_instance().debug(self._i2c_bus)
-            self._i2c_sensor = cl_fact_i2c_sensor_sht.get_instance(self.o_active_sensor, self._i2c_bus, self.o_address)
+           
             self._send_i2c_start_command()
         except Exception as cx_error:
             cl_fact_logic_messenger().get_instance().handle_exception(cx_error)
