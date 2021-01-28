b79beb08c99dd54698adaa4d97cfc9b1c5d8ea05
DerBurgermeister <DerBurgermeister@pi-ager.org>
2021-01-28 15:09:06 +0100

    #87 Base64 program

--- opt/pi-ager/pi_ager_encrypt_password.py
+++ opt/pi-ager/pi_ager_encrypt_password.py
@@ -1,4 +1,5 @@
 import sys
+import base64
 from main.pi_ager_cx_exception import *
 from main.pi_ager_cl_logger import cl_fact_logger
 from messenger.pi_ager_cl_email_server import cl_fact_db_email_server
@@ -9,7 +10,18 @@ from main.pi_ager_cl_logger import cl_fact_logger
 
 cl_fact_logger.get_instance().debug('Encrypt password called.')
 
-smtp_password = sys.argv[1]
+import_smtp_password = sys.argv[1]
+use_base64 = sys.argv[2]
+
+if (use_base64 == 'base64'):
+    base64_bytes = base64_message.encode('ascii')
+    message_bytes = base64.b64decode(import_smtp_password)
+    smtp_password = message_bytes.decode('ascii')
+else:
+    smtp_password = import_smtp_password
+
+
+
 print("Password             = " + smtp_password)
 cl_fact_logger.get_instance().debug('Password = ' + smtp_password)
 crypt = cl_fact_help_crypt.get_instance()
