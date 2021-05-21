d873a80c12c13ae78a12664b262979e6cdf69723
DerBurgermeister <DerBurgermeister@pi-ager.org>
2021-05-21 06:47:39 +0200

    Image script. Use last Backup
    Change-Id: I0000000000000000000000000000000000000000

--- usr/local/bin/pi-ager_image.sh
+++ usr/local/bin/pi-ager_image.sh
@@ -105,7 +105,7 @@ if [ "$last_backup" = true ];
 	Pi_Ager_backup = $(ls -lrt $(find ./ -type f) | grep 'Backup' | tail -n 1 | cut -d: -f2- | head);
 	
 	echo "Backup file is" $Pi_Ager_backup
-	source_file = ${BACKUP_PFAD}/Pi_Ager_backup
+	source_file = ${BACKUP_PFAD}/$Pi_Ager_backup
 	echo "Backup path is" $source_file
 fi
 
