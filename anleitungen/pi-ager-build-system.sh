84d480728f0bbf5260a690f4d4f98880df1e0b72
phylax2020 <severin.hoffmann@t-online.de>
2025-03-05 19:09:52 +0100

    Pi-Ager system setup scripts

---
+++ anleitungen/pi-ager-build-system.sh
@@ -0,0 +1,250 @@
+#!/bin/bash
+export PIP_BREAK_SYSTEM_PACKAGES=1
+
+# set -x
+# trap read debug
+
+if [ ! -f  /usr/share/this_script.progress ] ; then
+    echo 1 >/usr/share/this_script.progress
+
+#    cat > /etc/systemd/system/this_script.service <<EOF
+#[Unit]
+#Description=this script service
+# After=NetworkManager.service
+#After=multi-user.target
+
+#[Service]
+#ExecStart=/home/pi/pi-ager-build-system.sh
+#StandardOutput=append:/home/pi/pi-ager-build-system.log
+#StandardError=append:/home/pi/pi-ager-build-system-error.log
+
+#[Install]
+#WantedBy=multi-user.target
+#EOF
+#    systemctl enable this_script
+    echo 2 >/usr/share/this_script.progress
+fi
+progress=$(</usr/share/this_script.progress)
+echo $progress
+
+case $progress in
+ 1)
+    echo "This should not happen!"
+    ;;
+ 2)
+    echo 3 >/usr/share/this_script.progress
+    printf "\nPerform apt update / full-upgrade, wait 10s to start. Reboot when done\n"
+    sleep 10
+    apt update
+    apt -y full-upgrade
+    printf "\nreboot, after reboot start this script with sudo again to continue system setup\n"
+    reboot
+    ;;
+ 3)
+    echo 4 >/usr/share/this_script.progress
+    printf "\nSetup system for Pi-Ager part 1, wait 10s to start. Reboot when done\n"
+    sleep 10
+    sed -i 's/^#PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config 
+    service ssh restart
+    printf "\nsetup serial port and locale\n"
+    echo "root:raspberry" | chpasswd 
+#    raspi-config nonint do_serial_hw 0
+#    raspi-config nonint do_serial_cons 1
+#    raspi-config nonint do_change_locale de_DE.UTF-8 UTF-8
+#    raspi-config nonint do_change_locale en_GB.UTF-8 UTF-8
+    
+    printf "\nsetup /boot/firmware/config.txt and cmdline.txt\n"
+sed -i '/^#dtparam=spi=on$/a \
+\
+# --------------------- pi-ager settings --------- \
+# Use Pi-Ager Pins 11/13 GPIO 17/27 for I2C \
+dtoverlay=i2c-gpio,bus=3,i2c_gpio_sda=17,i2c_gpio_scl=27 \
+# Use Pi-Ager Pin 16 for MCP3204 \
+dtoverlay=spi1-1cs,cs0_pin=16 \
+# -----------------------------------------------' /boot/firmware/config.txt
+
+    echo "enable_uart=1" >> /boot/firmware/config.txt
+    echo "dtoverlay=miniuart-bt" >>/boot/firmware/config.txt
+    echo "dtparam=uart0=on" >> /boot/firmware/config.txt
+    echo "#  force_turbo=1" >> /boot/firmware/config.txt
+
+    sed -i 's/fsck.repair=yes/fsck.mode=force fsck.repair=yes/' /boot/firmware/cmdline.txt
+    sed -i 's/rootwait/rootwait dwc_otg.fiq_fsm_mask=0x3/' /boot/firmware/cmdline.txt
+    
+    printf "/nConfigure Locales: enable 'de_DE.UTF-8 UTF-8' and 'en_GB.UTF-8 UTF-8'\n"
+    printf "Other Locales can also be enabled but only en_GB and de_DE are supported by Pi-Ager.\n"
+    printf "Reboots automatically when finished\n"
+    printf "After reboot start this script with sudo again to continue system setup\n"
+    read -p "Press enter to continue"
+    
+    dpkg-reconfigure locales
+
+    reboot
+    ;;
+ 4)
+    echo 5 >/usr/share/this_script.progress
+    printf "\nSetup system for Pi-Ager part 2, wait 10s to start. Reboot when done\n"
+    sleep 10
+    echo "i2c-dev" >> /etc/modules
+    touch /etc/modprobe.d/raspi-blacklist.conf
+    apt -y install git
+    
+    printf "\nclone Pi-Ager software from github repository\n"
+    git clone --depth=1 -b entwicklung https://github.com/Tronje-the-Falconer/Pi-Ager
+    
+    cd Pi-Ager
+    cp /home/pi/Pi-Ager/boot/firmware/setup.txt /boot/firmware/
+    
+    printf "\ninstall php\n"
+    apt -y install php-fpm php-cli
+    apt -y install php
+    apt -y install php-common php-sqlite3
+    apt -y install php-mbstring php-zip php-curl
+    
+    printf "\ninstall lighttpd\n"
+    apt -y install lighttpd
+    sed -i 's%/var/www/html%/var/www%' /etc/lighttpd/lighttpd.conf
+    sed -i '/server.modules += (/a \
+"mod_auth", \
+"mod_authn_file",' /etc/lighttpd/lighttpd.conf
+
+    printf "\ncopy Pi-Ager from local folder to final destinations\n"
+    cp -r /home/pi/Pi-Ager/opt/* /opt/
+    cp -r /home/pi/Pi-Ager/var/* /var/
+    usermod -G www-data -a pi
+#    usermod –G gpio –a www-data
+    chown -R www-data:www-data /var/www
+    chmod -R 755 /var/www
+    chmod +x /opt/ATC_MiThermometer/ATC_xxxxxx.py
+    
+    printf "\ncreate .htcredentials\n"
+    user="pi-ager"
+    realm="Pi-Ager"
+    password="raspberry"
+    digest="$( printf "%s:%s:%s" "$user" "$realm" "$password" | md5sum | awk '{print $1}' )"
+        
+	rm /var/.htcredentials
+	echo "$user:$realm:$digest" > /var/.htcredentials
+    printf "\nPasswort webgui gesetzt\n"
+    
+    printf "\nconfigure lighttpd\n"
+    cp -rf /home/pi/Pi-Ager/etc/lighttpd/conf-available/05-auth.conf /etc/lighttpd/conf-available/
+    cp -rf /home/pi/Pi-Ager/etc/lighttpd/conf-available/15-fastcgi-php-fpm.conf /etc/lighttpd/conf-available/
+    lighty-enable-mod auth
+    lighty-enable-mod fastcgi-php-fpm
+    service lighttpd force-reload
+    
+    printf "\ninstall necessary modules\n"
+    apt -y install python3-smbus2
+    apt -y install sqlite3
+    apt -y install python3-pip
+    pip3 install pi-sht1x
+    apt -y install libgd-dev
+    apt -y install libssl-dev
+    apt -y install fswebcam
+    apt -y install python3-cryptography
+    apt -y install uuid-runtime
+    
+    printf "\ninstall wiringPi and RPi.GPIO\n"
+    cd /tmp
+    apt -y purge wiringpi
+    wget  https://github.com/phylax2020/WiringPi/releases/download/3.16/wiringpi_3.16_armhf.deb
+    apt -y install ./wiringpi_3.16_armhf.deb
+    
+    apt -y purge python3-RPi.GPIO
+    pip3 uninstall --yes RPi.GPIO
+    wget https://github.com/phylax2020/RPi.GPIO/releases/download/v0.8.6/python3-rpi.gpio_0.8.6-1_armhf.deb
+    apt -y install ./python3-rpi.gpio_0.8.6-1_armhf.deb
+
+    printf "\ninstall additional modules\n"
+    apt -y install lsof
+    pip3 install nextion
+    pip3 install pyserial-asyncio
+
+    apt -y install zip unzip
+    apt -y install python3-schedule
+    apt -y install python3-paho-mqtt
+    
+    cp -rf /home/pi/Pi-Ager/etc/rc.local /etc/
+    chmod +x /etc/rc.local
+    
+    printf "\ndisable start of pi-ager service in rc.local for next reboot\n"
+    sed -i 's/systemctl/#systemctl/' /etc/rc.local
+    
+    cp /home/pi/Pi-Ager/usr/local/bin/* /usr/local/bin/
+    chmod +x /usr/local/bin/*
+    chmod +x /var/*.sh
+    
+    cd /home/pi
+    cp /home/pi/Pi-Ager/etc/sudoers.d/99_pi-ager /etc/sudoers.d/
+    chmod 440 /etc/sudoers.d/99_pi-ager
+    
+    printf "\ncreate virtual interface wlan0\n"
+    iw dev wlan0 interface add wlan1 type __ap
+    
+    printf "\nconfigure access point with networkmanager\n"
+    nmcli con delete PI_AGER_AP
+    nmcli con add type wifi ifname wlan1 mode ap con-name PI_AGER_AP ssid pi-ager
+    nmcli con modify PI_AGER_AP 802-11-wireless.band bg
+    nmcli con modify PI_AGER_AP 802-11-wireless.channel 6
+    nmcli con modify PI_AGER_AP 802-11-wireless-security.key-mgmt wpa-psk
+ # nmcli con modify PI_AGER_AP 802-11-wireless-security.proto rsn
+ # nmcli con modify PI_AGER_AP 802-11-wireless-security.group ccmp
+ # nmcli con modify PI_AGER_AP 802-11-wireless-security.pairwise ccmp
+    nmcli con modify PI_AGER_AP 802-11-wireless-security.psk 1234567890
+    nmcli con modify PI_AGER_AP ipv4.addr 10.0.0.1/24
+    nmcli con modify PI_AGER_AP ipv4.method shared
+    nmcli con modify PI_AGER_AP ipv6.method disabled
+    nmcli con up PI_AGER_AP
+    
+    printf "\nInstall nodogsplash captive portal\n"
+    apt -y install iptables
+    apt -y install libmicrohttpd-dev
+    git clone https://github.com/nodogsplash/nodogsplash.git
+    cd ./nodogsplash
+    make
+    make install
+    cp -rf /home/pi/Pi-Ager/etc/nodogsplash/* /etc/nodogsplash/
+    chmod +x /usr/bin/nodogsplash
+    chmod +x /usr/bin/ndsctl
+    
+    printf "\nInstall bluetooth fo Xiaomi temp/hum sensor\n"
+    apt -y install libglib2.0-dev
+    pip3 install bluepy requests
+    apt -y install bluetooth libbluetooth-dev
+    
+    printf "\nPatch btle.py to stop intermittend errors\n"
+    sed -i '/self._helper.wait()/a \
+            time.sleep(0.1)' /usr/local/lib/python3.11/dist-packages/bluepy/btle.py
+
+    printf "\nchange some owner and rw rights\n"
+    
+    chmod 666 /var/www/logs/logfile.txt
+    chmod 755 /var/www/logs/
+    chmod 664 /var/www/config/pi-ager.sqlite3
+    chown -R www-data:www-data /var/www/config/
+    chmod 777 /var/www/config/
+    
+    printf "\ncopy pi-ager service files\n"
+    cp /home/pi/Pi-Ager/lib/systemd/system/* /lib/systemd/system/
+    systemctl daemon-reload
+    
+    printf "\ncopy fswebcam\n"
+    cp /home/pi/Pi-Ager/usr/bin/fswebcam /usr/bin/
+    chmod +x /usr/bin/fswebcam
+    cp /home/pi/Pi-Ager/usr/share/man/man1/fswebcam.1.gz /usr/share/man/man1/
+    
+    printf "\nclosing_actions\n"
+    
+#    systemctl disable this_script
+    rm -f /usr/share/this_script.progress /etc/systemd/system/this_script.service
+    rm -rf /home/pi/Pi-Ager
+    
+    echo "Edit now /boot/firmware/setup.txt file !"
+    echo "After editing and saving setup.txt start script pi-ager-finalize-build.sh with sudo to activate system configuration from data in setup.txt "
+    echo "rebooting now"
+    
+#   systemctl enable setup_pi-ager.service    
+    reboot
+    ;;
+esac
---
+++ anleitungen/pi-ager-finalize-build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ echo "Enable oi-ager service in rc.local after next reboot"
+ sed -i 's/#systemctl/systemctl/' /etc/rc.local
+ 
+ echo "enable and activate setup.txt after reboot"
+ systemctl enable setup_pi-ager.service
+ echo "rebooting" 
+ reboot
